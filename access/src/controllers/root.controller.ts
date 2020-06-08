bc0c5bf3d812c700c8c8872a3ebee8642bbdfcfc
sepsepsep <git@i239.co>
2020-06-07 23:54:39 -0400

    Access service

---
+++ access/src/controllers/root.controller.ts
@@ -0,0 +1,27 @@
+import * as express from 'express'
+import { Request, Response } from 'express'
+import IControllerBase from 'interfaces/IControllerBase.interface'
+
+
+class RootController implements IControllerBase 
+{
+    public path = '/'
+    public router = express.Router()
+    
+    constructor() 
+    {
+        this.initRoutes()
+    }
+
+    public initRoutes() 
+    {
+        this.router.get('/', this.index)
+    }
+
+    index = (req: Request, res: Response) => 
+    {
+        res.send("")
+    }
+}
+
+export default RootController
