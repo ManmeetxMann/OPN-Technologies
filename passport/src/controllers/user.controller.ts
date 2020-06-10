1369b56ab0680b80c8ff474aca3368f604f629bc
sepsepsep <git@i239.co>
2020-06-10 01:06:15 -0400

    Updated API calls

--- passport/src/controllers/user.controller.ts
+++ passport/src/controllers/user.controller.ts
@@ -19,19 +19,19 @@ class UserController implements IControllerBase
 
     public initRoutes() 
     {
-        this.router.post(this.path + '/check', this.add)
-        this.router.post(this.path + '/attestation/add', this.add)
+        this.router.post(this.path + '/status/get', this.check)
+        this.router.post(this.path + '/attestation/update', this.update)
     }
 
     check = (req: Request, res: Response) => 
     {
-        if (!Validation.validate(["currentStatusToken"], req, res))
+        if (!Validation.validate(["statusToken"], req, res))
         {
             return
         }
 
-        console.log(req.body.attestationId);
-        console.log(req.body.answer);
+        // console.log(req.body.attestationId);
+        // console.log(req.body.answer);
         const response = 
         {
             data : {
@@ -39,13 +39,14 @@ class UserController implements IControllerBase
                 updatedBadge : "green",
                 validUntil : (new Date(new Date().getTime() + 60 * 60 * 24 * 1000)).toISOString()
             },
+            serverTimestamp: (new Date()).toISOString(),
             status : "complete"
         }
 
         res.json(response);
     }
 
-    add = (req: Request, res: Response) => 
+    update = (req: Request, res: Response) => 
     {
         if (!Validation.validate(["attestationId", "answer"], req, res))
         {
