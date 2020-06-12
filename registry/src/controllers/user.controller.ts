20cf25d633fe7313ade2859ba17cebfbd78ce68d
sepsepsep <git@i239.co>
2020-06-12 08:08:17 -0400

    No push added as wel

--- registry/src/controllers/user.controller.ts
+++ registry/src/controllers/user.controller.ts
@@ -17,6 +17,7 @@ class UserController implements IControllerBase
     public initRoutes() 
     {
         this.router.post(this.path + '/add', this.add)
+        this.router.post(this.path + '/addNoPush', this.addNoPush)
     }
 
     add = (req: Request, res: Response) => 
@@ -26,6 +27,16 @@ class UserController implements IControllerBase
             return
         }
 
+        const response = 
+        {
+            status : "complete"
+        }
+
+        res.json(response);
+    }
+
+    addNoPush = (req: Request, res: Response) => 
+    {
         const response = 
         {
             status : "complete"
