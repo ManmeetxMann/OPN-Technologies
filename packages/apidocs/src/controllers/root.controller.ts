04fd2116810473795922479c9e4b931a382c573a
sepsepsep <git@i239.co>
2020-07-06 19:02:59 -0400

    Beginings of Swagger viewer

--- packages/apidocs/src/controllers/root.controller.ts
+++ packages/apidocs/src/controllers/root.controller.ts
@@ -2,10 +2,13 @@ import * as express from 'express'
 import { Request, Response } from 'express'
 import IControllerBase from '../../../common/src/interfaces/IControllerBase.interface'
 
+import swaggerJSDoc from "swagger-jsdoc"
+import swaggerUi from "swagger-ui-express"
+
 
 class RootController implements IControllerBase 
 {
-    public path = '/'
+    public path = "/"
     public router = express.Router()
     
     constructor() 
@@ -15,7 +18,35 @@ class RootController implements IControllerBase
 
     public initRoutes() 
     {
-        this.router.get('/', this.index)
+        // Swagger definition
+        const swaggerDefinition = 
+        {
+            swagger: '2.0',
+            info: 
+            {
+                title: "OPN REST API Documentation",
+                version: "1.0.0",
+                description: "OPN API docs using Open API / Swagger",
+            },
+            host: "localhost:3000",
+            basePath: "/api",
+        };
+
+        // options for the swagger docs
+        const options = 
+        {
+            // import swaggerDefinitions
+            swaggerDefinition,
+            // path to the API docs
+            apis: ["./docs/*.yaml"],
+        };
+        // initialize swagger-jsdoc
+        const swaggerSpec = swaggerJSDoc(options);
+
+        // use swagger-Ui-express for your app documentation endpoint
+        this.router.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+        this.router.get("/", this.index)
     }
 
     index = (req: Request, res: Response) => 
