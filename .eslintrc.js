5bede4ea5c9cdfa0e523792cac63d571d3d95242
Lyrold <lyrold.careto@gmail.com>
2020-07-07 00:22:33 -0400

    #3 - Support prettier and cleanup registry/user-controller

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,16 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',  // Specifies ESLint parser
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-    ],
-    plugins: ['@typescript-eslint'],
-    parserOptions: {
-        ecmaVersion: 2019,  // Allows parsing of modern ECMA features
-        sourceType: 'module',  // Allows for the use of imports
-    },
-    rules: {
-        "@typescript-eslint/no-explicit-any": 2
-    },
-};
+  parser: '@typescript-eslint/parser', // Specifies ESLint parser
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2019, // Allows parsing of modern ECMA features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 2,
+  },
+}
--- packages/registry/src/controllers/user.controller.ts
+++ packages/registry/src/controllers/user.controller.ts
@@ -1,61 +1,49 @@
 import * as express from 'express'
-import { Request, Response } from 'express'
+import {Request, Response} from 'express'
 import IControllerBase from '../../../common/src/interfaces/IControllerBase.interface'
 
-import Validation from '../../../common/src/utils/validation'
+import DataStore from '../../../common/src/data/datastore'
+import {RegistrationModel} from '../../../common/src/data/registration'
+import {RegistrationType} from '../../../common/src/schemas/registration'
 
-import DataStore from "../../../common/src/data/datastore"
-import { RegistrationModel } from "../../../common/src/data/registration"
-import { RegistrationType } from "../../../common/src/schemas/registration"
+class UserController implements IControllerBase {
+  public path = '/user'
+  public router = express.Router()
 
-class UserController implements IControllerBase 
-{
-    public path = '/user'
-    public router = express.Router()
-    
-    constructor()
-    {
-        this.initRoutes()
+  constructor() {
+    this.initRoutes()
+  }
+
+  public initRoutes() {
+    this.router.post(this.path + '/add', this.add)
+    this.router.post(this.path + '/addNoPush', this.addNoPush)
+  }
+
+  add = (req: Request, res: Response) => {
+    // Create DataStore
+    const datastore = new DataStore()
+
+    // Create
+    const registration = new RegistrationModel(datastore)
+    registration
+      .add({
+        type: RegistrationType.User,
+        pushToken: req.body.registrationToken,
+      })
+      .then(() =>
+        res.json({
+          status: 'complete',
+        }),
+      )
+  }
+
+  addNoPush = (req: Request, res: Response) => {
+    const response = {
+      status: 'complete',
     }
 
-    public initRoutes() 
-    {
-        this.router.post(this.path + '/add', this.add)
-        this.router.post(this.path + '/addNoPush', this.addNoPush)
-    }
-
-    add = async (req: Request, res: Response) => 
-    {
-        // Create DataStore
-        const datastore = new DataStore()
-
-        // Create
-        const registration = new RegistrationModel(datastore)
-    
-        // Add
-        const id1 = await registration.add({
-            type: RegistrationType.User,
-            pushToken: req.body.registrationToken
-        })
-
-
-        const response = 
-        {
-            status : "complete"
-        }
-
-        res.json(response);
-    }
-
-    addNoPush = (req: Request, res: Response) => 
-    {
-        const response =
-        {
-            status : "complete"
-        }
-
-        res.json(response);
-    }
+    res.json(response)
+  }
 }
 
 export default UserController
