3f455244cd60a2c8f5b6dc42c615941566f34b7e
sepsepsep <git@i239.co>
2020-07-08 19:32:05 -0400

    Removed the local package for now

--- packages/registry/src/server.ts
+++ packages/registry/src/server.ts
@@ -2,6 +2,7 @@ import App from '../../common/src/express/app'
 
 import * as bodyParser from 'body-parser'
 import loggerMiddleware from '../../common/src/middlewares/logger'
+// import loggerMiddleware from "@opn/common/src/middlewares/logger"
 
 import AdminController from './controllers/admin.controller'
 import UserController from  './controllers/user.controller'
