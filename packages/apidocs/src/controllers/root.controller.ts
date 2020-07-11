9ef4ea15a19b2c84a91086e2c6a886c064455832
sepsepsep <git@i239.co>
2020-07-11 18:20:29 -0400

    Config added to apidocs

--- packages/apidocs/src/controllers/root.controller.ts
+++ packages/apidocs/src/controllers/root.controller.ts
@@ -20,47 +20,32 @@ class RootController implements IControllerBase
 
     public initRoutes()
     {
-        // // Swagger definition
-        // const swaggerDefinition = 
-        // {
-        //     openapi: '3.0.1',
-        //     info: 
-        //     {
-        //         title: "OPN REST API Documentation",
-        //         version: "1.0.0",
-        //         description: "OPN API docs using Open API / Swagger",
-        //     },
-        //     servers: [
-        //         {
-        //             url: "https://registry-dot-opn-platform-dev.nn.r.appspot.com",
-        //             description: "Production server"
-        //         },
-        //         {
-        //             url: "http://localhost:5006",
-        //             description: "Development server"
-        //         }
-        //     ]
-        // };
-
-        // // options for the swagger docs
-        // const options = 
-        // {
-        //     // import swaggerDefinitions
-        //     swaggerDefinition,
-        //     // path to the API docs
-        //     apis: ["./src/docs/openapi.yaml"],
-        //     explorer: true
-        // };
-        // // initialize swagger-jsdoc
-        // const swaggerSpec = swaggerJSDoc(options);
-
-        // console.log(swaggerSpec)
-
-        // // use swagger-Ui-express for your app documentation endpoint
-        // this.router.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
+        // Swagger definition
         const factory = new SwaggerServiceFactory({
             services: [
+                new SwaggerService({
+                    openApiVersion: "3.0.1",
+                    info: {
+                        title: "Config API",
+                        version: "1.0.0",
+                        description: "OPN API docs using Open API / Swagger"
+                    },
+                    servers: [
+                        {
+                            url: "https://config-dot-opn-platform-dev.nn.r.appspot.com",
+                            description: "Production Server"
+                        },
+                        {
+                            url: "https://config-staging-dot-opn-platform-dev.nn.r.appspot.com",
+                            description: "Staging Server"
+                        },
+                        {
+                            url: "http://localhost:5001",
+                            description: "Development Server"
+                        }
+                    ],
+                    yamlPath: "config.yaml"
+                }),
                 new SwaggerService({
                     openApiVersion: "3.0.1",
                     info: {
