23a15b4bcd1ed0d20cb28bcc48a09e07ec67e17c
Lyrold <lyrold.careto@gmail.com>
2020-07-14 18:42:17 -0400

    #58 - Passport - Support `firestore.FieldValue.serverTimestamp()` when creating a passport

--- packages/passport/src/services/passport-service.ts
+++ packages/passport/src/services/passport-service.ts
@@ -3,6 +3,7 @@ import DataStore from '../../../common/src/data/datastore'
 import {ResourceNotFoundException} from '../../../common/src/exceptions/resource-not-found-exception'
 import {IdentifiersModel} from '../../../common/src/data/identifiers'
 import moment from 'moment'
+import {firestore} from 'firebase-admin'
 
 export class PassportService {
   private dataStore = new DataStore()
@@ -10,15 +11,29 @@ export class PassportService {
   private identifierRepository = new IdentifiersModel(this.dataStore)
 
   create(status: PassportStatuses = PassportStatuses.Pending): Promise<Passport> {
-    const now = moment()
-    return this.identifierRepository.getUniqueValue('status').then((statusToken) =>
-      this.passportRepository.add({
-        status,
-        statusToken,
-        validFrom: now.toISOString(),
-        validUntil: now.add(24, 'hours').toISOString(),
-      }),
-    )
+    return this.identifierRepository
+      .getUniqueValue('status')
+      .then((statusToken) =>
+        this.passportRepository.add({
+          status,
+          statusToken,
+          validFrom: firestore.FieldValue.serverTimestamp(),
+          validUntil: null,
+        }),
+      )
+      .then(({validFrom, ...passport}) => ({
+        ...passport,
+        // @ts-ignore
+        validFrom: new firestore.Timestamp(validFrom.seconds, validFrom.nanoseconds)
+          .toDate()
+          .toISOString(),
+      }))
+      .then(({validFrom, validUntil, ...passport}) => ({
+        ...passport,
+        validFrom,
+        validUntil: moment(validFrom).add(24, 'hours').toISOString(),
+      }))
+      .then((passport) => this.passportRepository.update(passport))
   }
 
   findOneByToken(token: string): Promise<Passport> {
