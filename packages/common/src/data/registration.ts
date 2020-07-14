669168ea9d483a773d4264e11079c2365a331dbf
sepsepsep <git@i239.co>
2020-07-14 09:52:50 -0400

    Introducing Auth Link Request
    Introduce the call into the API

---
+++ packages/common/src/data/registration.ts
@@ -0,0 +1,20 @@
+import DataModel from "./datamodel.base"
+
+export enum RegistrationType
+{
+    User = "User",
+    Admin = "Admin"
+}
+
+export interface RegistrationSchema
+{
+    id: string,
+    type: RegistrationType,
+    pushToken: string
+}
+
+export class RegistrationModel extends DataModel<RegistrationSchema>
+{
+    readonly rootPath = "registration"
+    readonly zeroSet = []
+}
--- packages/common/src/data/user.ts
+++ packages/common/src/data/user.ts
@@ -1,5 +1,13 @@
 import DataModel from '../../../common/src/data/datamodel.base'
 
+export type AdminProfile = {
+  approvalUsed: boolean
+  email: string
+  enabled: boolean
+  adminForLocationIds: string[]
+  superAdminForOrganizationIds: string[]
+}
+
 export type User = {
   id: string
   firstName: string
@@ -7,6 +15,8 @@ export type User = {
   birthYear: number
   base64Photo: string
   organizationIds?: string[]
+  admin?: AdminProfile
+  authUserId?: string
 }
 
 export class UserModel extends DataModel<User> {
