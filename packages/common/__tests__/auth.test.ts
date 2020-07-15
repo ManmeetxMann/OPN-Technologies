e1307dcdfa7351ded369e0d424fc54df97413f3d
sepsepsep <git@i239.co>
2020-07-14 21:40:37 -0400

    Added auth service and tests

---
+++ packages/common/__tests__/auth.test.ts
@@ -0,0 +1,18 @@
+import {AuthService} from '../src/service/auth/auth-service'
+import { doesNotMatch } from 'assert';
+
+describe("auth tests", () => {
+    test("auth user creation", async (done) => {
+        const authService = new AuthService()
+        const userCreated = await authService.createUser("sep@stayopn.com")
+        expect(userCreated)
+        done()
+    });
+
+    test("auth email login sending", async (done) => {
+        const authService = new AuthService()
+        const userCreated = await authService.sendEmailSignInLink("sep@stayopn.com")
+        expect(userCreated)
+        done()
+    });
+});
--- packages/common/src/service/auth/auth-service.ts
+++ packages/common/src/service/auth/auth-service.ts
@@ -1,4 +1,26 @@
+import { FirebaseManager, firebaseAdmin } from "../../utils/firebase"
+
 export class AuthService {
-    // TODO: implement Firebase Auth here...
-    // TODO: perhaps add a factory like messaging...
+    private readonly firebaseAuth = FirebaseManager.getInstance().getAdmin().auth()
+
+    async createUser(email: string) : Promise<boolean> {
+        try {
+            const user = await this.firebaseAuth.createUser({email: email})
+            console.log(user.uid)
+            return true
+        }
+        catch {
+            return false
+        }
+    }
+
+    async sendEmailSignInLink(email:string) : Promise<void> {
+        const signInLink = await this.firebaseAuth.generateSignInWithEmailLink(email, {
+            url: "https://www.stayopn.com/DONE"
+        })
+
+        console.log(`Sending url: ${signInLink}`)
+    }
+
+    // TODO: verify a token
 }
