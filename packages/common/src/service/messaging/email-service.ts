065f6c967cee1bdea457d135c3b65e48031c83e7
sepsepsep <git@i239.co>
2020-07-14 23:51:01 -0400

    Update email-service.ts

--- packages/common/src/service/messaging/email-service.ts
+++ packages/common/src/service/messaging/email-service.ts
@@ -1,4 +1,3 @@
-// import SibApiV3Sdk, {SendersApiApiKeys} from 'sib-api-v3-typescript'
 import fetch from 'node-fetch'
 
 export interface MailInfo {
@@ -8,16 +7,12 @@ export interface MailInfo {
 }
 
 export abstract class Mail {
-    // private instance : SibApiV3Sdk.SMTPApi
     private recipient: MailInfo
     private static readonly APIKEY = "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"
     
     protected abstract templateId : number
 
     constructor(recipient: MailInfo) {
-        // const smtpAPI = new SibApiV3Sdk.SMTPApi()
-        // smtpAPI.setApiKey(0, Mail.APIKEY)
-        // smtpAPI.setApiKey(1, Mail.APIKEY)
         this.recipient = recipient
     }
 
@@ -33,6 +28,8 @@ export abstract class Mail {
             //     'X-Mailin-custom': 'custom_header_1:custom_value_1|custom_header_2:custom_value_2'
             // }
         }
+
+        // FYI: Their Node and Typescript libraries are garbage!
 
         await fetch('https://api.sendinblue.com/v3/smtp/email', {
             method: 'post',
