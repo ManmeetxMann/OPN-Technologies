59eba092a98689a1538617d516ce5d7aa8cce17b
sepsepsep <git@i239.co>
2020-07-15 01:29:02 -0400

    Continuing the implementation
    Just a quick check-in. NOT FINISHED... Draft.

--- packages/enterprise/src/controllers/admin.controller.ts
+++ packages/enterprise/src/controllers/admin.controller.ts
@@ -1,11 +1,15 @@
 import * as express from 'express'
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import IControllerBase from '../../../common/src/interfaces/IControllerBase.interface'
 
 import Validation from '../../../common/src/utils/validation'
 
 import { AuthService } from '../../../common/src/service/auth/auth-service'
-import {AuthLinkRequest} from '../models/auth-link-request'
+import { AuthLinkRequest } from '../models/auth-link-request'
+import { actionSucceed } from '../../../common/src/utils/response-wrapper'
+import { AdminApprovalService } from '../../../common/src/service/user/admin-service'
+import { UnauthorizedException } from '../../../common/src/exceptions/unauthorized-exception'
+import { UserService } from '../../../common/src/service/user/user-service'
 
 class AdminController implements IControllerBase
 {
@@ -27,26 +31,48 @@ class AdminController implements IControllerBase
         this.router.post(this.path + '/billing/config', this.billingConfig)
     }
 
-    authSignInLinkRequest = async (req: Request, res: Response) => 
-    {
-        const {
-            email,
-            connectedId
-        } = req.body as AuthLinkRequest
-        
-        // Create the user if not created
-        const userCreated = await this.authService.createUser(email)
+    authSignInLinkRequest = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const {
+                email,
+                connectedId
+            } = req.body as AuthLinkRequest
 
-        // Send the email
-        const link = await this.authService.sendEmailSignInLink(email)
+            // Check if we have approval for this admin
+            const adminApprovalService = new AdminApprovalService()
+            const approval = await adminApprovalService.findOneByEmail(email)
+            if (approval === null || approval.expired === true)
+            {
+                throw new UnauthorizedException("Invalid Operation")
+            }
 
+            // Get connected Id and name + last initial
+            const userService = new UserService()
+            const connectedUser = await userService.findOneById(connectedId)
+            if (connectedUser == null)
+            {
+                throw new UnauthorizedException("Invalid Operation")
+            }
 
-        const response = 
-        {
-            status : "succeeded"
+            // We good now...
+
+            // We have it so let's mark it as expired
+            // adminApprovalService.updateExpiry(approval.id, true) // TODO: DO THIS ON APPROVED
+            
+            DO ABOVE AFTER approval AND CREATE LANDING PAGE FOR APPROVED AND APP REDIRECT...
+
+            // Create the user if not created
+            const userCreated = await this.authService.createUser(email)
+
+            // Send the email
+            const name = [connectedUser.firstName,connectedUser.lastNameInitial].join(" ")
+            const link = await this.authService.sendEmailSignInLink(email, name)
+            
+            res.json(actionSucceed());
+        } 
+        catch (error) {
+            next(error)
         }
-
-        res.json(response);
     }
 
     authSignIn = (req: Request, res: Response) => 
