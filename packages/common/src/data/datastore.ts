199e601955f5db31485b629c3c66e3adebae32d7
sepsepsep <git@i239.co>
2020-07-14 21:40:15 -0400

    Abstract out main Firebase setup to it's own manager
    And then pull it out of datastore class

--- packages/common/src/data/datastore.ts
+++ packages/common/src/data/datastore.ts
@@ -1,5 +1,4 @@
-import admin, { ServiceAccount } from "firebase-admin"
-import serviceAccount from "../../__secrets__/opn-platform-dev-firebase-adminsdk.json"
+import { FirebaseManager, firebaseAdmin } from "../utils/firebase"
 import { FirestoreSimple } from "@firestore-simple/admin"
 
 class DataStore
@@ -8,19 +7,13 @@ class DataStore
     private static readonly rootPath = "/"
 
     // Properties
-    private readonly firestore : admin.firestore.Firestore
+    private readonly firestore : firebaseAdmin.firestore.Firestore
     readonly firestoreORM : FirestoreSimple
-    readonly firestoreAdmin = admin
+    readonly firestoreAdmin = FirebaseManager.getInstance().getAdmin()
 
     constructor()
     {
         // Initialize Firestore
-        
-        // Needed when called from tests.. to ensure that we initialize it only once
-        if (!this.firestoreAdmin.apps.length)
-        {
-            this.firestoreAdmin.initializeApp({ credential: this.firestoreAdmin.credential.cert(serviceAccount as ServiceAccount) })
-        }
         this.firestore = this.firestoreAdmin.firestore()
         this.firestoreORM = new FirestoreSimple(this.firestore)
     }
---
+++ packages/common/src/utils/firebase.ts
@@ -0,0 +1,33 @@
+import admin, { ServiceAccount } from "firebase-admin"
+import serviceAccount from "../../__secrets__/opn-platform-dev-firebase-adminsdk.json"
+
+export class FirebaseManager
+{
+    // Properties
+    private readonly firestore : admin.firestore.Firestore
+    private readonly admin = admin
+    private static __instance : FirebaseManager = null
+
+    constructor()
+    {
+        // Needed when called from tests.. to ensure that we initialize it only once
+        if (!this.admin.apps.length)
+        {
+            this.admin.initializeApp({ credential: this.admin.credential.cert(serviceAccount as ServiceAccount) })
+        }
+        // this.firestore = this.admin.firestore()
+    }
+
+    getAdmin() {
+        return this.admin
+    }
+
+    static getInstance() {
+        if (FirebaseManager.__instance === null) {
+            FirebaseManager.__instance = new FirebaseManager()
+        }
+        return FirebaseManager.__instance
+    }
+}
+
+export { admin as firebaseAdmin }
