166443f084127477a4739f6d22ceeab4f0a82793
David Walker <davidw@evenset.com>
2020-07-20 14:17:18 -0400

    ts fix

--- packages/common/src/interfaces/IControllerBase.interface.ts
+++ packages/common/src/interfaces/IControllerBase.interface.ts
@@ -1,11 +1,5 @@
-import {Router} from 'express'
-
 interface IControllerBase {
   initRoutes(): void
-}
-
-export interface IRouteController extends IControllerBase {
-  router: Router
 }
 
 export default IControllerBase
