64283bcc88fd022a9153d25c987ebbfa192eeda2
Lyrold <lyrold.careto@gmail.com>
2020-07-21 15:37:34 -0400

    #110 - Fix `AccessStatsModel.createdAt` type

--- packages/access/src/repository/access-stats.repository.ts
+++ packages/access/src/repository/access-stats.repository.ts
@@ -1,10 +1,11 @@
 import DataModel from '../../../common/src/data/datamodel.base'
 import {AccessStats} from '../models/access-stats'
+import {firestore} from 'firebase-admin'
 
 export type AccessStatsModel = AccessStats & {
   id
   locationId: string
-  createdAt: unknown
+  createdAt: firestore.Timestamp
 }
 export class AccessStatsRepository extends DataModel<AccessStatsModel> {
   public readonly rootPath = 'access-stats'
