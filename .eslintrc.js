f799509f825e11d1283c7cbc045a2ee10fff16d6
David Walker <davidw@evenset.com>
2020-07-21 11:17:01 -0400

    Allow tslint disable comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,6 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-explicit-any': 2,
     '@typescript-eslint/no-unused-vars': ['error', {ignoreRestSiblings: true}],
-    '@typescript-eslint/ban-ts-comment': 'warn',
+    '@typescript-eslint/ban-ts-comment': 'off',
   },
 }
