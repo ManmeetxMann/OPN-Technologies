3ab695cbd55612b0ba94ff6113088d29222994fd
David Walker <davidw@evenset.com>
2020-08-01 16:42:58 -0400

    move type

--- packages/access/src/models/attendance.ts
+++ packages/access/src/models/attendance.ts
@@ -8,9 +8,5 @@ export type UserAccess = {
 export type Attendance = {
   id?: string
   date: string
-  accesses: {
-    userId: string
-    enteredAt: string
-    exitAt?: string
-  }[]
+  accesses: UserAccess[]
 }
