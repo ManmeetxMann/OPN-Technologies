3eadb9bb9fcc57277e800af8f454c51138dd0740
David Walker <davidw@evenset.com>
2020-08-05 12:28:02 -0400

    use access

--- packages/access/src/models/trace.ts
+++ packages/access/src/models/trace.ts
@@ -1,13 +1,10 @@
+import type {Access} from './access'
+
 export type Trace = {
   id?: string
   userId: string
   severity: 'YELLOW' | 'RED'
   date: string
   durations: number[]
-  accesses: {
-    userId: string
-    enteredAt: string
-    exitAt?: string
-    locationId: string
-  }[]
+  accesses: Access[]
 }
