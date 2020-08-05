c6e9c3767f2c2fc30921e35449b891eccb1bc989
David Walker <davidw@evenset.com>
2020-08-05 13:43:46 -0400

    use severity constants

--- packages/access/src/models/trace.ts
+++ packages/access/src/models/trace.ts
@@ -1,9 +1,9 @@
 import type {Access} from './access'
-
+import {PassportStatuses} from '../../../passport/src/models/passport'
 export type Trace = {
   id?: string
   userId: string
-  severity: 'YELLOW' | 'RED'
+  severity: PassportStatuses.Caution | PassportStatuses.Stop
   date: string
   durations: number[]
   accesses: Access[]
