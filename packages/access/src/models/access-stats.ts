6aad44c5919736b6cff54f7c701be74bb021211d
Lyrold <lyrold.careto@gmail.com>
2020-08-26 22:47:32 -0400

    Init live-view stats through `/admin/access/stats` endpoint

--- packages/access/src/models/access-stats.ts
+++ packages/access/src/models/access-stats.ts
@@ -1,4 +1,11 @@
 export type AccessStats = {
   peopleOnPremises: number
   accessDenied: number
+  asOfDate: Date
+  exposures: number
+  pendingPassports: number
+  proceedPassports: number
+  cautionPassports: number
+  stopPassports: number
+  checkInsPerHour: {date: Date; count: number}[]
 }
