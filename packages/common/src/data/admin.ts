716521dc5c6de9f2b707ca6c1cc95e0bf54063f9
David Walker <davidw@evenset.com>
2020-08-31 12:38:21 -0400

    remove duplicated flag

--- packages/common/src/data/admin.ts
+++ packages/common/src/data/admin.ts
@@ -13,7 +13,6 @@ export type AdminApproval = {
   id: string
   expired: boolean
   profile: AdminProfile
-  showReporting: boolean
 }
 
 export class AdminApprovalModel extends DataModel<AdminApproval> {
