079e157c064cd8b6c187787861867b05ee4f7cf0
Lyrold <lyrold.careto@gmail.com>
2020-09-07 20:01:52 -0400

    Add debug logs for incoming request

--- packages/common/src/middlewares/logger.ts
+++ packages/common/src/middlewares/logger.ts
@@ -2,6 +2,7 @@ import {Request, Response, NextFunction} from 'express'
 
 const loggerMiddleware = (req: Request, resp: Response, next: NextFunction): void => {
   console.log('Request logged:', req.method, req.path)
+  console.log('Debug request:', req)
   next()
 }
 
