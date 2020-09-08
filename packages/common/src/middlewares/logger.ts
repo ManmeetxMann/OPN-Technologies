674d701fc28979b28f9484eb7b3531c13e3e0fd0
David Walker <davidw@evenset.com>
2020-09-08 12:30:54 -0400

    only log used info

--- packages/common/src/middlewares/logger.ts
+++ packages/common/src/middlewares/logger.ts
@@ -2,7 +2,8 @@ import {Request, Response, NextFunction} from 'express'
 
 const loggerMiddleware = (req: Request, resp: Response, next: NextFunction): void => {
   console.log('Request logged:', req.method, req.path)
-  console.log('Debug request:', req)
+  const {headers, params, query, body} = req
+  console.info('request details:', {params, query, body, auth: headers?.authorization})
   next()
 }
 
