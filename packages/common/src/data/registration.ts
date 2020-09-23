91ebaf63a7a83e2452c37a6c80e6462ea42a961c
sepsepsep <git@i239.co>
2020-09-22 23:26:17 -0400

    Cleanup

--- packages/common/src/data/registration.ts
+++ packages/common/src/data/registration.ts
@@ -3,18 +3,10 @@ import {Platform} from '../types/platform'
 
 export type Registration = {
   id: string
-  type: RegistrationType
   platform: Platform
   osVersion: string
   pushToken?: string
   userIds?: string[]
-}
-
-export type RegistrationType = 'user' | 'admin'
-
-export enum RegistrationTypes {
-  User = 'user',
-  Admin = 'admin',
 }
 
 export class RegistrationModel extends DataModel<Registration> {
