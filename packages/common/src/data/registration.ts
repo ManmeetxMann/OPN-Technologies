be87409f7eb458e779d334155f77a8fad77df249
sepsepsep <git@i239.co>
2020-09-22 22:38:57 -0400

    Move file to common

--- packages/common/src/data/registration.ts
+++ packages/common/src/data/registration.ts
@@ -1,17 +1,22 @@
 import DataModel from './datamodel.base'
+import {Platform} from '../types/platform'
 
-export enum RegistrationType {
-  User = 'User',
-  Admin = 'Admin',
-}
-
-export interface RegistrationSchema {
+export type Registration = {
   id: string
   type: RegistrationType
-  pushToken: string
+  platform: Platform
+  osVersion: string
+  pushToken?: string
 }
 
-export class RegistrationModel extends DataModel<RegistrationSchema> {
-  readonly rootPath = 'registration'
+export type RegistrationType = 'user' | 'admin'
+
+export enum RegistrationTypes {
+  User = 'user',
+  Admin = 'admin',
+}
+
+export class RegistrationModel extends DataModel<Registration> {
+  public readonly rootPath = 'registration'
   readonly zeroSet = []
 }
