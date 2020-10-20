d488d6b8d03fb6f4942fabb939f28af7d733ee24
Antonyan7 <you@example.com>
2020-10-20 23:04:29 +0400

    refactor: remove jquery, errors parts are commented

--- packages/reservation/src/static/js/test_results.js
+++ packages/reservation/src/static/js/test_results.js
@@ -1,63 +1,73 @@
-$(function() {
-    $( "#submitResults" ).submit(function( event ) {
-        // Stop form from submitting normally
-        event.preventDefault();
+document.addEventListener('DOMContentLoaded', event => {
+  const submitResultsForm = document.getElementById('submitResults')
+  const submitAgainBtn = document.getElementById("sendResultsAgain");
 
-        var data = {
-            "barCode":"TEST1000000005",
-            "result": "Positive",
-            "famEGene": "fam1",
-            "famCt": "fmct",
-            "calRed61RdRpGene": "A1000000005",
-            "calRed61Ct": "A1000000005",
-            "quasar670NGene": "A1000000005",
-            "quasar670Ct": "A1000000005",
-            "hexIC": "A1000000005",
-            "hexCt": "A1000000005"
-        };
+  submitResultsForm.addEventListener('submit', async event => {
+    event.preventDefault();
 
-        $.ajax({
-            'type': 'POST',
-            'url': "/admin/api/v1/send-and-save-test-results",
-            'contentType': 'application/json',
-            'data': JSON.stringify(data),
-            'dataType': 'json',
-            'success': function(){
-                $('#successMessage').modal()
-                $('#successMessage').modal('show')
-            },
-            'error':function( jqXHR,  textStatus, errorThrown){
-                if(jqXHR.status == 409){
-                    $('#confirmSendingAgain').modal()
-                    $('#confirmSendingAgain').modal('show')
-                }
-                console.log(jqXHR.status);
-                console.log(textStatus);
-            }
-        });
-    });
+    const data = {
+      barCode: 'TEST1000000005',
+      result: 'Positive',
+      famEGene: 'fam1',
+      famCt: 'fmct',
+      calRed61RdRpGene: 'A1000000005',
+      calRed61Ct: 'A1000000005',
+      quasar670NGene: 'A1000000005',
+      quasar670Ct: 'A1000000005',
+      hexIC: 'A1000000005',
+      hexCt: 'A1000000005',
+    }
 
-    $( "#sendResultsAgain" ).click(function( event ) {
+    try {
+      const raw = await fetch('/admin/api/v1/send-and-save-test-results', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
 
-        var data = {
-            "barCode":"TEST1000000005"
-        };
+      const content = await raw.json();
 
-        $.ajax({
-            'type': 'POST',
-            'url': "/admin/api/v1/send-test-results-again",
-            'contentType': 'application/json',
-            'data': JSON.stringify(data),
-            'dataType': 'json',
-            'success': function(){
-                $('#successMessage').modal()
-                $('#successMessage').modal('show')
-            },
-            'error':function( jqXHR,  textStatus, errorThrown){
-                console.log(jqXHR.status);
-                console.log(textStatus);
-                console.log(errorThrown);
-            }
-        });
-    });
+      $('#successMessage').modal()
+      $('#successMessage').modal('show')
+    } catch (e) {
+      console.log("jqXHR.status", e);
+      console.log("textStatus", e);
+
+      // @TODO Test console.log and make code below to work
+      // if (jqXHR.status == 409) {
+      //   $('#confirmSendingAgain').modal()
+      //   $('#confirmSendingAgain').modal('show')
+      // }
+
+    }
+
+  });
+
+  submitAgainBtn.addEventListener('click', async e => {
+    const data = {
+      barCode: 'TEST1000000005',
+    }
+
+    try {
+      const raw = await fetch('/admin/api/v1/send-test-results-again', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+
+      const content = await raw.json();
+      console.log(content);
+
+    } catch (e) {
+      console.log(e);
+    }
+
+  });
+
 });
