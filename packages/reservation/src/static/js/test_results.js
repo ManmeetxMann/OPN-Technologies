1acd6fc47f428a1c5f40c922059d36a4e48834f1
Antonyan7 <you@example.com>
2020-10-21 19:48:17 +0400

    refactor: remove jQuery part

--- packages/reservation/src/static/js/test_results.js
+++ packages/reservation/src/static/js/test_results.js
@@ -1,83 +1,99 @@
-document.addEventListener('DOMContentLoaded', event => {
+document.addEventListener('DOMContentLoaded', () => {
   const submitResultsForm = document.getElementById('submitResults')
-  const submitAgainBtn = document.getElementById("sendResultsAgain");
+  const submitAgainBtn = document.getElementById('sendResultsAgain')
+  const sendResultNoBtn = document.getElementById('sendResultNo')
+  const messageModal = document.getElementById('message')
+  const confirmSendingAgainModal = document.getElementById('confirmSendingAgain')
 
-  const getbyId = function( id ) { return document.getElementById( id ).value; };
-  const showAlertModal = function(title, message) {
-    $('#message .modal-title').html(title);
-    $('#message .modal-body').html(message);
-    $('#message').modal();
-    $('#message').modal('show');
+  const getValueById = (id) => document.getElementById(id).value
+
+  const findAncestor = (el, sel) => {
+    while ((el = el.parentElement) && !(el.matches || el.matchesSelector).call(el, sel)) return el
   }
-  submitResultsForm.addEventListener('submit', async event => {
-    event.preventDefault();
+
+  const openModal = (modal) => modal.classList.add('show-modal')
+  const closeModal = (modal) => modal.classList.close('show-modal')
+
+  const showAlertModal = function (title, message) {
+    messageModal.querySelector('.modal-title').innerHTML = title
+    messageModal.querySelector('.modal-body').innerHTML = message
+    openModal(messageModal)
+  }
+
+  const createCloseModal = (elem) => {
+    closeModal(findAncestor(elem, '.show-modal'))
+  }
+
+  submitResultsForm.addEventListener('submit', async (event) => {
+    event.preventDefault()
 
     const data = {
-      barCode: getbyId('barCode'),
-      result: getbyId('result'),
-      famEGene: getbyId('famEGene'),
-      famCt: getbyId('famCt'),
-      calRed61RdRpGene: getbyId('calRed61RdRpGene'),
-      calRed61Ct: getbyId('calRed61Ct'),
-      quasar670NGene: getbyId('quasar670NGene'),
-      quasar670Ct: getbyId('quasar670Ct'),
-      hexIC: getbyId('hexIC'),
-      hexCt: getbyId('hexCt'),
+      barCode: getValueById('barCode'),
+      result: getValueById('result'),
+      famEGene: getValueById('famEGene'),
+      famCt: getValueById('famCt'),
+      calRed61RdRpGene: getValueById('calRed61RdRpGene'),
+      calRed61Ct: getValueById('calRed61Ct'),
+      quasar670NGene: getValueById('quasar670NGene'),
+      quasar670Ct: getValueById('quasar670Ct'),
+      hexIC: getValueById('hexIC'),
+      hexCt: getValueById('hexCt'),
     }
 
     try {
       const response = await fetch('/admin/api/v1/send-and-save-test-results', {
         method: 'POST',
         headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data)
-      });
+        body: JSON.stringify(data),
+      })
 
-      const responseData = await response.json();
+      const responseData = await response.json()
 
       if (response.ok) {
-        showAlertModal("Success", responseData.data);
+        showAlertModal('Success', responseData.data)
       } else {
         if (response.status === 409) {
-          $('#confirmSendingAgain').modal();
-          $('#confirmSendingAgain').modal('show');
-        }else{
-          showAlertModal("Failed", responseData.status.message);
+          openModal(confirmSendingAgainModal)
+        } else {
+          showAlertModal('Failed', responseData.status.message)
         }
       }
     } catch (e) {
-      showAlertModal("Failed", "Something went wrong. Please try after sometime.");
+      showAlertModal('Failed', 'Something went wrong. Please try after sometime.')
     }
+  })
 
-  });
-
-  submitAgainBtn.addEventListener('click', async e => {
+  submitAgainBtn.addEventListener('click', async (e) => {
     const data = {
-      barCode: getbyId('barCode'),
+      barCode: getValueById('barCode'),
     }
 
     try {
       const response = await fetch('/admin/api/v1/send-test-results-again', {
         method: 'POST',
         headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
       })
 
-      const responseData = await response.json();
+      const responseData = await response.json()
+      closeModal(confirmSendingAgainModal)
       if (response.ok) {
-        showAlertModal("Success", responseData.data);
-      }else{
-        showAlertModal("Failed", responseData.status.message);
+        showAlertModal('Success', responseData.data)
+      } else {
+        showAlertModal('Failed', responseData.status.message)
       }
     } catch (e) {
-      showAlertModal("Failed", "Something went wrong. Please try after sometime.");
+      showAlertModal('Failed', 'Something went wrong. Please try after sometime.')
     }
-
-  });
-
-});
+  })
+  ;[...document.getElementsByClassName('close')].forEach((closBtn) =>
+    closBtn.addEventListener('click', ({target}) => createCloseModal(target)),
+  )
+  sendResultNoBtn.addEventListener('click', ({target}) => createCloseModal(target))
+})
