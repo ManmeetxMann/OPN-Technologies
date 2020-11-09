13ce685e4ac3b0ebe730dd86b42b53855c87a30e
David Walker <davidw@evenset.com>
2020-11-09 13:44:09 -0500

    clickable link

--- packages/enterprise/src/services/email-service.ts
+++ packages/enterprise/src/services/email-service.ts
@@ -6,7 +6,7 @@ export class EmailService {
   sendGroupReport(email: string, name: string, publicUrl: string): Promise<unknown> {
     return this.emailService.send({
       to: [{email, name}],
-      textContent: `Report available at ${publicUrl}`,
+      textContent: `Click <a href="${publicUrl}">here</a> to download your report`,
       subject: 'OPN Report',
       sender: {
         email: 'no-reply@email.stayopn.net',
