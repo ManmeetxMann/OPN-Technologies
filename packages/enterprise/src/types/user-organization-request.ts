d014fcd91692557618e12c7447b66e7ebb110ab6
Aleksander Margiste <happyfamilylife11@gmail.com>
2020-11-23 20:07:46 +0100

    update user info by admin, update GET users endpoint to use query orgId

--- packages/enterprise/src/types/user-organization-request.ts
+++ packages/enterprise/src/types/user-organization-request.ts
@@ -1,3 +1,9 @@
+import {PageableRequestFilter} from '../../../common/src/types/request'
+
 export type ConnectOrganizationRequest = {
   organizationId: string
+}
+
+export type UsersByOrganizationRequest = PageableRequestFilter & {
+  organizationId?: string
 }
