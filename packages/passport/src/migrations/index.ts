6402efaf6473d89645fdf1d4ec5f3beedba9174c
Pavel <pavelsvintsitsky@gmail.com>
2020-11-23 20:05:47 +0200

    small fixes (replaced class with function, code style change)

--- packages/passport/src/migrations/index.ts
+++ packages/passport/src/migrations/index.ts
@@ -1,18 +1,15 @@
 import {AttestationsEdit} from './attestationsEdit'
+const attestationMigration = new AttestationsEdit()
+const [method] = process.argv.slice(2)
 
-class Migrations {
-  public attestationMigration = new AttestationsEdit()
-  constructor() {
-    const [method] = process.argv.slice(2)
-
-    this.attestationMigration[method]()
-      .then(() => {
-        console.log('migrations completed successfully')
-      })
-      .catch((e) => {
-        console.error(`migrations error: ${e.message}`)
-      })
-  }
+function runMigration(): void {
+  attestationMigration[method]()
+    .then(() => {
+      console.log('migrations completed successfully')
+    })
+    .catch((e) => {
+      console.error(`migrations error: ${e.message}`)
+    })
 }
 
-new Migrations()
+runMigration()
