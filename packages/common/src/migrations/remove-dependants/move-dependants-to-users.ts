9326859702d50f0921c4ee72c3b9c2f907e8e3a1
David Walker <davidw@evenset.com>
2020-11-24 18:12:29 -0500

    add migrated flag

--- packages/common/src/migrations/remove-dependants/move-dependants-to-users.ts
+++ packages/common/src/migrations/remove-dependants/move-dependants-to-users.ts
@@ -33,6 +33,7 @@ export default async function runMigration(): Promise<void> {
                 organizationIds: user.organizationIds ?? [],
                 email: user.email ?? null,
                 delegates: [user.id],
+                migrated: true,
                 // no admin
                 // no authUserId
               })
