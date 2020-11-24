5b44cb16b1e2390b797f8c7da82acb55e2eb78b0
David Walker <davidw@evenset.com>
2020-11-24 12:51:38 -0500

    add down migrations

---
+++ packages/common/src/migrations/remove-dependants/move-users-to-dependants.ts
@@ -0,0 +1,46 @@
+import {UserModel, UserDependantModel} from '../../data/user'
+import DataStore from '../../data/datastore'
+const PAGE_SIZE = 200
+export default async function runMigration(): Promise<void> {
+  const ds = new DataStore()
+  const ORM = ds.firestoreORM
+  const userModel = new UserModel(ds)
+  let pageIndex = 0
+  while (true) {
+    const allUsers = await userModel.fetchAllWithPagination(pageIndex, PAGE_SIZE)
+    if (allUsers.length === 0) {
+      break
+    }
+    await Promise.all(
+      allUsers.map(async (user) => {
+        if (!user.delegates?.length) {
+          console.log(`User ${user.id} is not a dependant, continuing`)
+          return
+        }
+        const parent = user.delegates[0]
+
+        return ORM.runTransaction(async (tx) => {
+          const deleteRef = ORM.collection({
+            path: `users/`,
+          }).docRef(user.id)
+          const createRef = ORM.collection({
+            path: `users/${parent}/dependants/`,
+          }).docRef(user.id)
+          tx.delete(deleteRef).create(createRef, {
+            registrationId: null,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            base64Photo: '',
+            organizationIds: user.organizationIds ?? [],
+            email: user.email ?? null,
+            // no admin
+            // no authUserId
+          })
+        })
+      }),
+    )
+    pageIndex += 1
+  }
+}
+
+runMigration()
---
+++ packages/common/src/migrations/remove-dependants/remove-delegate-array.ts
@@ -0,0 +1,30 @@
+import {UserModel} from '../../data/user'
+import DataStore from '../../data/datastore'
+import {FieldValue} from '@google-cloud/firestore'
+
+const PAGE_SIZE = 200
+export default async function runMigration(): Promise<void> {
+  console.log('running migration')
+  const model = new UserModel(new DataStore())
+  let pageIndex = 0
+  while (true) {
+    const allUsers = await model.fetchAllWithPagination(pageIndex, PAGE_SIZE)
+    if (allUsers.length === 0) {
+      break
+    }
+    console.log(`Updating page ${pageIndex + 1} with ${allUsers.length} users in it`)
+    await Promise.all(
+      allUsers.map((user) => {
+        if (user.delegates?.length) {
+          throw 'Cannot remove a delegate array that still has users'
+        }
+        return model.updateProperty(user.id, 'delegates', FieldValue.delete)
+      }),
+    )
+    console.log(`Update complete`)
+    pageIndex += 1
+  }
+  console.log('migration complete')
+}
+
+runMigration()
