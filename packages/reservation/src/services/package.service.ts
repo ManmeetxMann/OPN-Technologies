07c321aae3e67191fc24f955781e24a59125a201
Maksym Perepelitsa <perepelitsa.maksym.external@kreios.lu>
2020-12-02 22:40:44 +0200

    Feat: 833 fixed PR commetns

--- packages/reservation/src/services/package.service.ts
+++ packages/reservation/src/services/package.service.ts
@@ -23,6 +23,10 @@ export class PackageService {
   async getByPackageCode(packageCode: string): Promise<PackageBase> {
     const result = await this.packageRepository.findWhereEqual('packageCode', packageCode)
 
+    if (result.length > 1) {
+      console.log(`More than 1 result for the packageCode ${packageCode}`)
+    }
+
     return result[0]
   }
 
