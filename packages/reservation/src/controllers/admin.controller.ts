235f72adbc9a38e3d68f51ef1ff04c3e5901856b
Harpreet Gill <harpreet@shopthing.com>
2020-12-03 19:52:01 -0500

    Fixed Lint error

--- packages/reservation/src/controllers/admin.controller.ts
+++ packages/reservation/src/controllers/admin.controller.ts
@@ -142,7 +142,6 @@ class AdminController implements IControllerBase {
                 }),
               ])
             }
-
           } else {
             notFoundBarcodes.push(row)
           }
