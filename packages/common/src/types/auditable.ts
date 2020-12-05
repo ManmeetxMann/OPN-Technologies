e2631208a5028726b4ac295011542697919ca76b
David Walker <davidw@evenset.com>
2020-12-05 11:11:13 -0500

    add migration timeestamp

--- packages/common/src/types/auditable.ts
+++ packages/common/src/types/auditable.ts
@@ -5,6 +5,7 @@ export type Auditable<T extends number | Date | string = Date> = {
     // TODO to be flatten
     createdAt: firestore.Timestamp
     updatedAt: firestore.Timestamp
+    migratedAt?: firestore.Timestamp
   }
   updatedBy: string //TODO: handle with authenticated userId
 }
