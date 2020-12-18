80c3a80c80b342cb216be3da1a2756277e893964
Tsovak Harutyunyan <tsovakh@gmail.com>
2020-12-18 05:15:47 +0400

    chore: move views folder to src

--- packages/reservation/src/server.ts
+++ packages/reservation/src/server.ts
@@ -41,6 +41,7 @@ const app = new App({
 //Attach handlebar only for Reservation Server
 app.app.engine('handlebars', exphbs())
 app.app.set('view engine', 'handlebars')
+app.app.set('views', path.join(__dirname, 'views'))
 app.app.use('/static', express.static(path.join(__dirname, 'static')))
 
 app.listen()
