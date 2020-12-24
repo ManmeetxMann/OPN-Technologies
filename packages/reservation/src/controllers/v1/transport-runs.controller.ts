de6debd6dcdca65a727354921786e526d606bdf9
Tsovak Harutyunyan <tsovakh@gmail.com>
2020-12-24 05:52:45 +0400

    feat: finialize transport runs create endpoint

---
+++ packages/reservation/src/controllers/v1/transport-runs.controller.ts
@@ -0,0 +1,42 @@
+import IControllerBase from "../../../../common/src/interfaces/IControllerBase.interface";
+import {Handler, Router} from "express";
+import {adminAuthMiddleware} from "../../../../common/src/middlewares/admin.auth";
+import {TransportRunsService} from "../../services/transport-runs.service";
+import {actionSucceed} from "../../../../common/src/utils/response-wrapper";
+
+class TransportRunsController implements IControllerBase {
+    public path = '/reservation/admin/api/v1/transport-runs'
+    public router = Router()
+    private transportRunsService = new TransportRunsService()
+
+
+    constructor() {
+        this.initRoutes()
+    }
+
+    public initRoutes(): void {
+        const innerRouter = Router({mergeParams: true})
+        innerRouter.post(this.path + '/', this.createTransportRun)
+
+        this.router.use('/', innerRouter)
+    }
+
+    createTransportRun:Handler = async (req, res, next): Promise<void> => {
+        try {
+            const {transportDateTime, driverName} = req.body as {transportDateTime: string, driverName: string}
+
+            const transportRuns = await this.transportRunsService.create(transportDateTime, driverName)
+
+            res.json(
+                actionSucceed({
+                    id: transportRuns.id,
+                }),
+            )
+        } catch (error) {
+            console.log(error);
+            next(error)
+        }
+    }
+}
+
+export default TransportRunsController;
---
+++ packages/reservation/src/models/transport-runs.ts
@@ -0,0 +1,11 @@
+export type TransportRunsDbModel = {
+    id: string
+    transportRunId: string
+    transportDateTime: string
+    driverName: string
+}
+
+export type TransportRunsIdentifier = {
+    id: string
+    transportRunId: string
+}
