3893df7fdead6450351b404d1a33ed2ed7af7e7f
Tsovak Harutyunyan <tsovakh@gmail.com>
2020-12-25 04:27:25 +0400

    feat: make filtering by date

--- packages/reservation/src/models/transport-runs.ts
+++ packages/reservation/src/models/transport-runs.ts
@@ -5,7 +5,8 @@ export type TransportRunsBase = {
 }
 
 export type TransportRunsDbModel = TransportRunsBase & {
-  id: string
+  id: string,
+  transportDate: string
 }
 
 export type TransportRunsIdentifier = {
