e420a409f92a43f8e7f7ac1c5905d98822f774cf
Harpreet Gill <harpreet@shopthing.com>
2021-01-13 14:41:51 -0500

    Updates to Cancel endpoint to use FireStore ID and cancel appointment on both Firestore and Acuity

--- packages/reservation/src/respository/pcr-test-results-repository.ts
+++ packages/reservation/src/respository/pcr-test-results-repository.ts
@@ -1,4 +1,4 @@
-import DataModel from '../../../common/src/data/datamodel.base'
+import DataModel, { DataModelFieldMapOperatorType } from '../../../common/src/data/datamodel.base'
 import DataStore from '../../../common/src/data/datastore'
 import {PCRTestResultDBModel} from '../models/pcr-test-results'
 
@@ -12,5 +12,34 @@ export class PCRTestResultsRepository extends DataModel<PCRTestResultDBModel> {
 
   public async save(data: Omit<PCRTestResultDBModel, 'id'>): Promise<PCRTestResultDBModel> {
     return this.add(data)
+  }
+
+  async getWaitingPCRResultsByAppointmentId(appointmentId: string): Promise<PCRTestResultDBModel[]> {
+    const pcrTestResultsQuery = [
+      {
+        map: '/',
+        key: 'appointmentId',
+        operator: DataModelFieldMapOperatorType.Equals,
+        value: appointmentId,
+      },
+      {
+        map: '/',
+        key: 'waitingResult',
+        operator: DataModelFieldMapOperatorType.Equals,
+        value: true,
+      },
+    ]
+    const pcrTestResults = await this.findWhereEqualInMap(
+      pcrTestResultsQuery,
+    )
+
+    if (pcrTestResults.length > 1) {
+      //CRITICAL
+      console.log(
+        `getWaitingPCRResultsByAppointmentId: Multiple test results found with Appointment Id: ${appointmentId} `,
+      )
+    }
+
+    return pcrTestResults
   }
 }
