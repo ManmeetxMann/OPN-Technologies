b6ad8caaa319a696d5614c144f3fec95ad29cb77
Harpreet Gill <harpreet@shopthing.com>
2021-01-22 12:50:02 -0500

    Added Availability Times Model

---
+++ packages/reservation/src/models/available-times.ts
@@ -0,0 +1,24 @@
+export type AvailableTimes = {
+  id: string
+  label: string
+  slotsAvailable: number
+}
+
+export type AvailableSlotsUIDTO = {
+  times: AvailableTimes[]
+  totalSlotsAvailable: number
+}
+
+export const slotUiDTOResponse = (
+  times: AvailableTimes[]
+): AvailableSlotsUIDTO => {
+
+  const totalSlotsAvailable = times.reduce((totalSlots, time)=>{
+    return totalSlots+=time.slotsAvailable
+  }, 0)
+
+  return {
+    times,
+    totalSlotsAvailable: totalSlotsAvailable,
+  }
+}
