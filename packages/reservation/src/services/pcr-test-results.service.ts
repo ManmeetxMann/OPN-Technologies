2f28e03fd969cf76ba1271fa68afee7be92c7828
tsovak <tsovak@stayopn.com>
2021-01-27 04:52:39 +0400

    fix: remove duplicate

--- packages/reservation/src/services/pcr-test-results.service.ts
+++ packages/reservation/src/services/pcr-test-results.service.ts
@@ -22,7 +22,6 @@ import {
 import {
   CreateReportForPCRResultsResponse,
   PCRResultActions,
-  PCRResultActionsAllowedResend,
   PCRTestResultData,
   PCRTestResultDBModel,
   PCRTestResultEmailDTO,
