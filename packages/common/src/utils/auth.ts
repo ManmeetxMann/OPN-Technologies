02e9ce27852153ded84217710eb71a4ad57a6186
David Walker <davidw@evenset.com>
2021-01-28 13:47:07 -0500

    initial build

--- packages/common/src/utils/auth.ts
+++ packages/common/src/utils/auth.ts
@@ -1,5 +1,6 @@
 import {AdminProfile} from '../data/admin'
+import {User} from '../data/user'
 
-export const getAdminId = (user: {admin: AdminProfile}): string => user.admin.authUserId
+export const getAdminId = (user: User): string => (user.admin ? (user.authUserId as string) : null)
 
-export const getIsLabUser = (user: {admin: AdminProfile}): boolean => user.admin.isLabUser
+export const getIsLabUser = (user: User): boolean => (user.admin as AdminProfile).isLabUser
