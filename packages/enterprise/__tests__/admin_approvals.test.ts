3c3f082545fb1cac0004e63c1dc855c59a19f7d6
Harpreet Gill <harpreet@shopthing.com>
2021-01-28 12:44:24 -0500

    Lint fix

--- packages/enterprise/__tests__/admin_approvals.test.ts
+++ packages/enterprise/__tests__/admin_approvals.test.ts
@@ -1,7 +1,6 @@
 import {AdminApprovalService} from '../../common/src/service/user/admin-service'
 
 describe('admin tests', () => {
-
   test('Admin Approval > Find', async () => {
     // Get approval
     const adminApprovalService = new AdminApprovalService()
