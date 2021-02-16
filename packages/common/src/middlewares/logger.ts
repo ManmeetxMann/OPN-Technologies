5562462af7bb232cd61f9a6dda496852c438ce82
Harpreet Gill <harpreet@shopthing.com>
2021-02-16 15:22:08 -0500

    Added Bunyan Logger

--- packages/common/src/middlewares/logger.ts
+++ packages/common/src/middlewares/logger.ts
@@ -1,8 +1,26 @@
 import {Request, Response, NextFunction} from 'express'
+import bunyan from 'bunyan'
+
+// Imports the Google Cloud client library for Bunyan
+import { LoggingBunyan } from '@google-cloud/logging-bunyan'
+
+// Creates a Bunyan Cloud Logging client
+const loggingBunyan = new LoggingBunyan();
 
 const loggerMiddleware = (req: Request, resp: Response, next: NextFunction): void => {
+  const logger = bunyan.createLogger({
+    // The JSON payload of the log as it appears in Cloud Logging
+    // will contain "name": "my-service"
+    name: 'my-service',
+    streams: [
+      // Log to the console at 'info' and above
+      {stream: process.stdout, level: 'info'},
+      // And log to Cloud Logging, logging at 'info' and above
+      loggingBunyan.stream('info'),
+    ],
+  });
   const {headers, params, query, body} = req
-  console.info(
+  logger.info(
     'Request logged:',
     req.method,
     req.path,
