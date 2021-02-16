74aa791c7bea72c736b904a02d0e06e1e903d04c
Harpreet Gill <harpreet@shopthing.com>
2021-02-16 15:23:01 -0500

    Added BarCode Prefix for Test ENV

--- packages/reservation/src/respository/appointments-barcode-sequence.ts
+++ packages/reservation/src/respository/appointments-barcode-sequence.ts
@@ -1,6 +1,7 @@
 import {AppoinmentBarCodeSequenceDBModel} from '../models/appointment'
 import DataModel from '../../../common/src/data/datamodel.base'
 import DataStore from '../../../common/src/data/datastore'
+import { Config } from '../../../common/src/utils/config'
 
 export class AppointmentsBarCodeSequence extends DataModel<AppoinmentBarCodeSequenceDBModel> {
   public rootPath = 'appointment-barcode-sequence'
@@ -10,7 +11,7 @@ export class AppointmentsBarCodeSequence extends DataModel<AppoinmentBarCodeSequ
   }
 
   public async getNextBarCode(): Promise<null | AppoinmentBarCodeSequenceDBModel> {
-    const prefix = 'A'
+    const prefix = Config.get('BARCODE_SEQ_PREFIX') ?? 'TEST'
     return this.get(prefix)
       .then((sequence) =>
         !!sequence
