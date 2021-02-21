85d0bdb1aed3cf1afc0ea98b895b7fe83ec1d982
Harpreet Gill <harpreet@shopthing.com>
2021-02-21 11:29:12 -0500

    Updated Test for Status and Org

--- packages/reservation/__tests__/controllers/v1/admin/appointment.controller.ispec.ts
+++ packages/reservation/__tests__/controllers/v1/admin/appointment.controller.ispec.ts
@@ -6,29 +6,72 @@ jest.mock('../../../../../common/src/middlewares/authorization')
 
 const dateForAppointments = '2020-02-05'
 const dateTimeForAppointment1 = `${dateForAppointments}T07:00:00`
+const organizationId = "TEST1"
 
 describe('AdminAppointmentController', () => {
   beforeAll(async () => {
     await createAppointment({
       dateTime: dateTimeForAppointment1,
       dateOfAppointment: 'February 05, 2020',
+      appointmentStatus: 'InTransit'
     })
     await createAppointment({
       dateTime: dateTimeForAppointment1,
       dateOfAppointment: 'February 05, 2020',
+      appointmentStatus: 'InProgress'
+    })
+    await createAppointment({
+      dateTime: dateTimeForAppointment1,
+      dateOfAppointment: 'February 05, 2020',
+      organizationId:organizationId,
+      appointmentStatus: 'InProgress'
+    })
+    await createAppointment({
+      dateTime: dateTimeForAppointment1,
+      dateOfAppointment: 'February 05, 2020',
+      organizationId:organizationId
     })
     await createAppointment({
       dateTime: `2020-02-01T07:00:00`,
       dateOfAppointment: 'February 01, 2020',
     })
+    await createAppointment({
+      dateTime: `2020-02-01T08:00:00`,
+      dateOfAppointment: 'February 01, 2020',
+    })
+    await createAppointment({
+      dateTime: `2020-02-01T08:00:00`,
+      dateOfAppointment: 'February 01, 2020',
+      appointmentStatus: 'InProgress'
+    })
   })
 
   describe('get appointment list', () => {
-    test('get appointments by date of appointment successfully.', async (done) => {
+    test('get appointments by dateOfAppointment successfully.', async (done) => {
       const url = `/reservation/admin/api/v1/appointments?dateOfAppointment=${dateForAppointments}`
       const result = await request(server.app).get(url).set('authorization', 'bearer 10000')
       expect(result.status).toBe(200)
+      expect(result.body.data.length).toBe(3)
+      done()
+    })
+    test('get InTransit appointments by dateOfAppointment successfully.', async (done) => {
+      const url = `/reservation/admin/api/v1/appointments?dateOfAppointment=${dateForAppointments}&appointmentStatus=InTransit`
+      const result = await request(server.app).get(url).set('authorization', 'bearer 10000')
+      expect(result.status).toBe(200)
+      expect(result.body.data.length).toBe(1)
+      done()
+    })
+    test('get appointments by organizationId successfully.', async (done) => {
+      const url = `/reservation/admin/api/v1/appointments?organizationId=${organizationId}&dateOfAppointment=${dateForAppointments}`
+      const result = await request(server.app).get(url).set('authorization', 'bearer 10000')
+      expect(result.status).toBe(200)
       expect(result.body.data.length).toBe(2)
+      done()
+    })
+    test('get appointments by organizationId should fail for missing dateOfAppointment', async (done) => {
+      const url = `/reservation/admin/api/v1/appointments?organizationId=${organizationId}`
+      const result = await request(server.app).get(url).set('authorization', 'bearer 10000')
+      expect(result.status).toBe(400)
       done()
     })
   })
