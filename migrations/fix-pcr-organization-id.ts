792a9c2de2f0d4e716c33c7bf78e46973233164c
tsovak <tsovak@stayopn.com>
2021-02-22 21:10:04 +0400

    feat: create pcr-to-organization migration

---
+++ migrations/fix-pcr-organization-id.ts
@@ -0,0 +1,123 @@
+/**
+ * This script to copy Test Results to PCR Results Collection
+ */
+import {initializeApp, credential, firestore} from 'firebase-admin'
+import {Config} from '../packages/common/src/utils/config'
+
+const serviceAccount = JSON.parse(Config.get('FIREBASE_ADMINSDK_SA'))
+initializeApp({
+  credential: credential.cert(serviceAccount),
+})
+console.log(serviceAccount.project_id)
+
+const database = firestore()
+
+export enum ResultStatus {
+  Fulfilled = 'fulfilled',
+  Rejected = 'rejected',
+}
+
+type Result = {
+  status: ResultStatus
+  value: unknown
+}
+
+export async function promiseAllSettled(promises: Promise<unknown>[]): Promise<Result[]> {
+  return Promise.all(
+    promises.map((promise) =>
+      promise
+        .then((value) => ({
+          status: ResultStatus.Fulfilled,
+          value,
+        }))
+        .catch((error: unknown) => ({
+          status: ResultStatus.Rejected,
+          value: error,
+        })),
+    ),
+  )
+}
+
+async function updateTestResults(): Promise<Result[]> {
+  let offset = 0
+  let hasMore = true
+
+  const results: Result[] = []
+
+  while (hasMore) {
+    const pcrResultSnapshot = await database
+      .collection('pcr-test-results')
+      .where('organizationId', '==', '')
+      .offset(offset)
+      .limit(limit)
+      .get()
+
+    offset += pcrResultSnapshot.docs.length
+    hasMore = !pcrResultSnapshot.empty
+    //hasMore = false
+
+    for (const pcrResult of pcrResultSnapshot.docs) {
+      const promises = []
+      promises.push(updatePcrTestResult(pcrResult))
+      const result = await promiseAllSettled(promises)
+      results.push(...result)
+    }
+  }
+  return results
+}
+
+async function updatePcrTestResult(
+  snapshot: firestore.QueryDocumentSnapshot<firestore.DocumentData>,
+) {
+  try {
+    await snapshot.ref.set(
+      {
+        organizationId: null,
+        timestamps: {
+          migrations: {
+            fixPcrOrganizationId: firestore.FieldValue.serverTimestamp(),
+          },
+        },
+      },
+      {
+        merge: true,
+      },
+    )
+
+    console.info(`Successfully Changed organization to null for ${snapshot.id}`)
+  } catch (error) {
+    console.warn(error)
+    throw error
+  }
+}
+
+async function main() {
+  try {
+    console.log('Migration Starting')
+    const results = await updateTestResults()
+    results.forEach((result) => {
+      totalCount += 1
+      if (result.status === ResultStatus.Fulfilled) {
+        if (result.value) {
+          successCount += 1
+        }
+      } else {
+        failureCount += 1
+      }
+    })
+    console.log(`Succesfully updated ${successCount} `)
+  } catch (error) {
+    console.error('Error running migration', error)
+  } finally {
+    console.warn(`Failed updating ${failureCount} `)
+    console.log(`Total Appointments Processed: ${totalCount} `)
+  }
+}
+
+// Maximum batch size to query for
+let successCount = 0
+let failureCount = 0
+let totalCount = 0
+const limit = Number(Config.get('MIGRATION_DOCUMENTS_LIMIT')) || 500
+
+main().then(() => console.log('Script Complete \n'))
