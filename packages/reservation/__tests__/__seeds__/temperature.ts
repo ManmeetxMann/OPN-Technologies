e00be827bf2faf7cda589f05f5524977b6d41587
Harpreet Gill <harpreet@shopthing.com>
2021-02-23 23:45:33 -0500

    Added Basic Tests for  GET Temperature ENdpoint

---
+++ packages/reservation/__tests__/__seeds__/temperature.ts
@@ -0,0 +1,35 @@
+import {firestore} from 'firebase-admin'
+
+const database = firestore()
+const collectionName = "temperature"
+export const createTemperature = async (dataOverwrite: {
+  id: string,
+  createdAt: string,
+  organizationID: string,
+  userID: string
+}): Promise<void> => {
+  const data = {
+    organizationId: dataOverwrite.organizationID,
+    status: "proceed",
+    temperature: 37.1, 
+    timestamps:{
+      createdAt: firestore.Timestamp.fromDate(new Date(dataOverwrite.createdAt)),
+      updatedAt: null
+    },
+    userId: dataOverwrite.userID
+  }
+
+  await database.collection(collectionName).doc(dataOverwrite.id).set(data)
+  //console.log(`savedData.id: ${savedData}`)
+}
+
+export const deleteTemperatureByDateTime = async (dateTime: string): Promise<void> => {
+  const appointments_query = database
+    .collection(collectionName)
+    .where('timestamps.createdAt', '<=', firestore.Timestamp.fromDate(new Date(dateTime)))
+  await appointments_query.get().then(function (querySnapshot) {
+    querySnapshot.forEach(function (doc) {
+      doc.ref.delete()
+    })
+  })
+}
