95972409fdc057b78f2bfc27f16807882c41b923
Harpreet Gill <harpreet@shopthing.com>
2021-02-25 16:50:20 -0500

    Updated Result Sender to return Status

---
+++ packages/reservation/src/models/rapid-antigen-test-results.ts
@@ -0,0 +1,11 @@
+export enum RapidAntigenResultTypes {
+  DoNothing = 'DoNothing',
+  SendInconclusive = 'SendInconclusive',
+  SendNegative = 'SendNegative',
+  SendPositive = 'SendPositive',
+}
+
+export type RapidAntigenTestResultRequest = {
+  appointmentID: string
+  action: RapidAntigenResultTypes
+}
