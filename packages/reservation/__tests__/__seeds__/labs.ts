eef40f0f43dab2979448107e251648096e98a5d2
Harpreet Gill <harpreet@shopthing.com>
2021-02-24 19:52:40 -0500

    Added Tests for Lab

---
+++ packages/reservation/__tests__/__seeds__/labs.ts
@@ -0,0 +1,30 @@
+import {firestore} from 'firebase-admin'
+
+const database = firestore()
+const collectionName = 'labs'
+export const create = async (dataOverwrite: {
+  id: string
+  createdAt: string
+  userID: string
+}): Promise<void> => {
+  const data = {
+    name: 'Lab1',
+    timestamps: {
+      createdAt: firestore.Timestamp.fromDate(new Date(dataOverwrite.createdAt)),
+      updatedAt: null,
+    },
+  }
+
+  await database.collection(collectionName).doc(dataOverwrite.id).set(data)
+  //console.log(`savedData.id: ${savedData}`)
+}
+
+export const deleteAll = async (): Promise<void> => {
+  const appointments_query = database
+    .collection(collectionName)
+  await appointments_query.get().then(function (querySnapshot) {
+    querySnapshot.forEach(function (doc) {
+      doc.ref.delete()
+    })
+  })
+}
