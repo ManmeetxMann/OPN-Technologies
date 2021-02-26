819601fd8788830784c8b1aae17eb7153b3c14ec
Harpreet Gill <harpreet@shopthing.com>
2021-02-25 19:40:18 -0500

    Added Linting

--- functional-testing/__tests__/packages/reservation/rapid-antigen/rapid-antigen-post.js
+++ functional-testing/__tests__/packages/reservation/rapid-antigen/rapid-antigen-post.js
@@ -1,50 +1,43 @@
 const frisby = require('frisby');
-const helpers_common = require('helpers_common');
-const admin_tags_data = require('enterprise/admin_tags_data');
-const testProfile = require('test_profile');
+const helpersCommon = require('helpers_common');
 
 // Do setup first
 frisby.globalSetup({
-	request: {
-		headers:  helpers_common.headers()
-	}
+  request: {
+    headers: helpersCommon.headers(),
+  },
 });
-const reservationServiceUrl = process.env.RESERVATION_SERVICE_URL
+const reservationServiceUrl = process.env.RESERVATION_SERVICE_URL;
 
-const organizationId = testProfile.get().organizationId
-const userId = testProfile.get().userId
 /**
  * @group reservation-service
  * @group /reservation/admin/api/v1/rapid-antigen-test-results
  * @group rapid-antigen-create
  */
-describe('post:temperature', () => {
-    test('create stop passport by submitting temperature.',  () => {
-        return helpers_common.runAuthenticatedTest(frisby).then(function(token){
-            const url = `${reservationServiceUrl}/reservation/admin/api/v1/rapid-antigen-test-results`
-            return frisby
-                    .setup({
-                        request: {
-                            headers: {
-                                'Authorization': `Bearer ${token}`
-                            }
-                        }
-                    })
-                    .post(
-                        url,
-                        [
-                            {
-                                "appointmentID": 'PcCFDchvWyBCVnXMyGNk',
-                                "action": 'DoNothing'
-                            },
-                            {
-                                "appointmentID": 'cJXQ6JFKntMEuF33F7oG',
-                                "action": 'SendNegative'//DoNothing, SendInconclusive, SendNegative, SendPositive
-                            }
-                        ]
-                    )
-                    .inspectBody()
-                    .expect('status', 200)
-        })
-    })
-})
+describe('rapid-antigen-create', () => {
+  test('able to successfully create Rapid Antigen Results', () => {
+    return helpers_common.runAuthenticatedTest(frisby).then(function(token) {
+      const url = `${reservationServiceUrl}/reservation/admin/api/v1/rapid-antigen-test-results`;
+      return frisby
+          .setup({
+            request: {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            },
+          })
+          .post(url, [
+            {
+              appointmentID: 'PcCFDchvWyBCVnXMyGNk',
+              action: 'DoNothing',
+            },
+            {
+              appointmentID: 'cJXQ6JFKntMEuF33F7oG',
+              action: 'SendNegative', // DoNothing, SendInconclusive, SendNegative, SendPositive
+            },
+          ])
+          .inspectBody()
+          .expect('status', 200);
+    });
+  });
+});
