e50b977ccc2ddc7d4bd3467723faf811e6c6978a
Harpreet Gill <harpreet@shopthing.com>
2021-02-26 00:37:26 -0500

    Added Stub Implementation for Rapid Allergen Email Sender

--- packages/reservation/src/services/rapid-antigen-test-results.service.ts
+++ packages/reservation/src/services/rapid-antigen-test-results.service.ts
@@ -1,6 +1,7 @@
 //Common
 import DataStore from '../../../common/src/data/datastore'
 import {LogError, LogInfo} from '../../../common/src/utils/logging-setup'
+import {OPNPubSub} from '../../../common/src/service/google/pub_sub'
 
 //Repository
 import {AppointmentsRepository} from '../respository/appointments-repository'
@@ -18,8 +19,9 @@ export class RapidAntigenTestResultsService {
   private dataStore = new DataStore()
   private pcrTestResultsRepository = new PCRTestResultsRepository(this.dataStore)
   private appointmentsRepository = new AppointmentsRepository(this.dataStore)
-
-  async saveAndSendRapidAntigenTestTesults(
+  private pubSub = new OPNPubSub('rapid-alergen-test-result-topic')
+  
+  async saveRapidAntigenTestTesults(
     requestData: RapidAntigenTestResultRequest[],
     reqeustedBy: string,
   ): Promise<BulkOperationResponse[]> {
@@ -66,10 +68,18 @@ export class RapidAntigenTestResultsService {
           result: getResult(appointment.action),
           waitingResult: false,
         })
+
+        //Update Appointments
         await this.appointmentsRepository.changeStatusToReported(
           appointment.appointmentID,
           reqeustedBy,
         )
+
+        //Send Push Notification
+        this.pubSub.publish({
+          appointmentID:appointment.appointmentID
+        })
+
         LogInfo('saveAndSendRapidAntigenTestTesults.processAppointment', 'Success', appointmentData)
         return Promise.resolve({
           id: appointment.appointmentID,
@@ -97,5 +107,11 @@ export class RapidAntigenTestResultsService {
       requestData.map(async (appointment) => processAppointment(appointment)),
     )
     return results
+  }
+
+  async sendTestResultEmail(
+    appointmentID: string,
+  ): Promise<void> {
+    console.log(`Processed: ${appointmentID}`)
   }
 }
