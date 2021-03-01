b996ff55af9834fa0e729464721db78c861e294b
Harpreet Gill <harpreet@shopthing.com>
2021-02-28 23:07:40 -0500

    Fixed Linting

--- packages/reservation/src/services/rapid-antigen-test-results.service.ts
+++ packages/reservation/src/services/rapid-antigen-test-results.service.ts
@@ -71,9 +71,7 @@ export class RapidAntigenTestResultsService {
     )
 
     //Send Push Notification
-    const actionsWithNotifyEnabled = [
-      RapidAntigenResultTypes.SendPositive,
-    ]
+    const actionsWithNotifyEnabled = [RapidAntigenResultTypes.SendPositive]
     if (notify && actionsWithNotifyEnabled.includes(action)) {
       this.pubSub.publish({
         appointmentID: appointmentId,
