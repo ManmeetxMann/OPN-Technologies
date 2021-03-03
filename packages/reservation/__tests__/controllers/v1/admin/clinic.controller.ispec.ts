8659d289697ec913184116201236a7512370fbe3
Giorgi Gelashvili <g_gelashvili4@cu.edu.ge>
2021-03-03 19:29:33 +0400

    add clinic api tests

---
+++ packages/reservation/__tests__/controllers/v1/admin/clinic.controller.ispec.ts
@@ -0,0 +1,34 @@
+import request from 'supertest'
+
+import {app as server} from '../../../../src/app'
+jest.mock('../../../../../common/src/middlewares/authorization')
+
+describe('Clinics controller test', () => {
+  const headers = {
+    accept: 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: 'Bearer CorporateUserForTEST1',
+  }
+
+  test('should create clinic', async (done) => {
+    const url = `/reservation/admin/api/v1/clinics`
+    const response = await request(server.app).post(url).set(headers).send({
+      name: 'ClinicName',
+      address: 'ClinicAdress',
+      acuityUser: 'TestUser',
+      acuityPass: 'TestPass',
+    })
+
+    expect(response.status).toBe(200)
+    expect(response.body.data.id.length).toBeGreaterThan(0)
+    done()
+  })
+
+  test('should return list of clinics', async (done) => {
+    const url = `/reservation/admin/api/v1/clinics`
+    const response = await request(server.app).post(url).set(headers)
+
+    expect(response.status).toBe(200)
+    done()
+  })
+})
