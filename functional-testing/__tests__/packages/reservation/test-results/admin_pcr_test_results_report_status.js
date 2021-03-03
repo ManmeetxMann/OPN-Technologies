c8b875badcea0de006be0414c5a7f55b91eb454b
Harpreet Gill <harpreet@shopthing.com>
2021-03-02 23:14:23 -0500

    More Cleanup

--- functional-testing/__tests__/packages/reservation/test-results/admin_pcr_test_results_report_status.js
+++ functional-testing/__tests__/packages/reservation/test-results/admin_pcr_test_results_report_status.js
@@ -1,14 +1,6 @@
 const frisby = require('frisby');
-const tv4 = require('tv4');
-const moment = require('moment');
 const helpersCommon = require('helpersCommon');
-// const admin_tags_data = require('enterprise/admin_tags_data');
-const testProfile = require('test_profile');
-const schemaDefinations = require('reservation/test_result').schemaDefinations;
-const test_results_data = require('reservation/test_results');
 const reservationServiceUrl = process.env.RESERVATION_SERVICE_URL;
-// const timeZone = process.env.DEFAULT_TIME_ZONE
-const organizationId = testProfile.get().organizationId;
 
 // Do setup first
 frisby.globalSetup({
@@ -17,9 +9,6 @@ frisby.globalSetup({
   },
 });
 
-beforeAll(function() {
-
-});
 /**
  * @group reservation-service
  * @group /reservation/admin/api/v1/pcr-test-results-bulk/report-status
--- functional-testing/__tests__/packages/reservation/test-results/admin_pcr_test_results_test_run_put.js
+++ functional-testing/__tests__/packages/reservation/test-results/admin_pcr_test_results_test_run_put.js
@@ -1,45 +1,12 @@
 const frisby = require('frisby');
-const tv4 = require('tv4');
-const moment = require('moment');
 const helpersCommon = require('helpersCommon');
-// const admin_tags_data = require('enterprise/admin_tags_data');
-const testProfile = require('test_profile');
-const schemaDefinations = require('reservation/test_result').schemaDefinations;
-const test_results_data = require('reservation/test_results');
 const reservationServiceUrl = process.env.RESERVATION_SERVICE_URL;
-// const timeZone = process.env.DEFAULT_TIME_ZONE
-const organizationId = testProfile.get().organizationId;
 
 // Do setup first
 frisby.globalSetup({
   request: {
     headers: helpersCommon.headers(),
   },
-});
-
-beforeAll(function() {
-  // Add our custom expect handler
-  frisby.addExpectHandler('validateSchema', function(response) {
-    const jsonData = response.json;
-    const schema = {'$ref': 'TestResultList'};
-
-    tv4.addSchema('schema', schema);
-    tv4.addSchema('TestResultList', schemaDefinations.$TestResultList);
-    tv4.addSchema('TestResultResponse', schemaDefinations.$TestResultResponse);
-    tv4.addSchema('OPNStatus', schemaDefinations.$OPNStatus);
-
-    const validation = tv4.validateMultiple(jsonData, schema, true, true);
-    console.log(validation);
-
-    if (!validation.valid) {
-      console.log('Schema Validation failed');
-      let i;
-      for (i = 0; i < validation.errors.length; i++) {
-        console.log('param:' + validation.errors[i].dataPath + ', error:' + validation.errors[i].message);
-      }
-    }
-    expect(validation['valid']).toBe(true);
-  });
 });
 
 /**
