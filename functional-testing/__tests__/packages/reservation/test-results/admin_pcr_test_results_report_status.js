cc288e1fd221efbd1c1c0a5cad43a234014a882a
Harpreet Gill <harpreet@shopthing.com>
2021-03-02 23:05:01 -0500

    Report Status

---
+++ functional-testing/__tests__/packages/reservation/test-results/admin_pcr_test_results_report_status.js
@@ -0,0 +1,48 @@
+const frisby = require('frisby');
+const tv4 = require('tv4');
+const moment = require('moment');
+const helpers_common = require('helpers_common');
+//const admin_tags_data = require('enterprise/admin_tags_data');
+const testProfile = require('test_profile');
+const schemaDefinations = require('reservation/test_result').schemaDefinations;
+const test_results_data = require('reservation/test_results');
+const reservationServiceUrl = process.env.RESERVATION_SERVICE_URL
+//const timeZone = process.env.DEFAULT_TIME_ZONE
+const organizationId = testProfile.get().organizationId
+
+// Do setup first
+frisby.globalSetup({
+	request: {
+		headers:  helpers_common.headers()
+	}
+});
+
+beforeAll(function () {
+
+});
+/**
+ * @group reservation-service
+ * @group /reservation/admin/api/v1/pcr-test-results-bulk/report-status
+ * @group get-bulk-report-status
+ */
+describe('Get Report Status', () => {
+    test('Get Report Status', function () {
+        return helpers_common.runAuthenticatedTest(frisby).then(function(token){
+
+            const url = `${reservationServiceUrl}/reservation/admin/api/v1/pcr-test-results-bulk/report-status?reportTrackerId=rXVZ2iTC4aNmlDJV8FHj`
+            return frisby
+                .setup({
+                    request: {
+                        headers: {
+                            'Authorization': `Bearer ${token}`
+                        }
+                    }
+                })
+                .get(
+                    url
+                )
+                .expect('status', 200)
+                .inspectBody()
+        })
+    })
+})
