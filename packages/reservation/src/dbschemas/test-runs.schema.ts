7147176b1f4fd85db2a73316d6d6f0aca10b857a
Pavel Svintsitskiy <pavelsvintsitsky@gmail.com>
2021-03-12 17:14:50 +0200

    feat: update schema

--- packages/reservation/src/dbschemas/test-runs.schema.ts
+++ packages/reservation/src/dbschemas/test-runs.schema.ts
@@ -4,6 +4,7 @@ import {FirestoreTimestamp} from '../../../common/src/utils/joi-extensions'
 export default Joi.object({
   testRunId: Joi.string().required(),
   testRunDate: Joi.string().required(),
+  labId: Joi.string().required(),
   testRunDateTime: FirestoreTimestamp.isValid(),
   name: Joi.string().allow(''),
 })
