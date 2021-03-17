33989079f36229a059844bea41aab5850da03b42
Tsovak Harutyunyan <tsovak@stayopn.com>
2021-03-18 03:44:56 +0400

    feat: add labAssay support

--- packages/reservation/src/services/test-results.service.ts
+++ packages/reservation/src/services/test-results.service.ts
@@ -8,11 +8,12 @@ import {RapidAntigenTestResultsService} from './rapid-antigen-test-results.servi
 // templates
 import {PCRResultPDFStream} from '../templates/pcr-test-results'
 import {RapidAntigenPDFStream} from '../templates/rapid-antigen'
+import {LabService} from './lab.service'
 
 export class TestResultsService {
   private pcrTestResultsService = new PCRTestResultsService()
   private rapidAntigenTestResultsService = new RapidAntigenTestResultsService()
-
+  private labService = new LabService()
   // async sendFax(testResults: TestResultsDTOForEmail, faxNumber: string): Promise<string> {
   //   const resultDateRaw = testResults.resultDate
   //   const date = new Date()
@@ -31,8 +32,10 @@ export class TestResultsService {
   ): Promise<Stream> {
     switch (testResult.testType) {
       case TestTypes.PCR:
+        const lab = await this.labService.findOneById(testResult.labId)
+
         return PCRResultPDFStream(
-          {...testResult, ...appointment},
+          {...testResult, ...appointment, labAssay: lab.assay},
           this.pcrTestResultsService.getPDFType(appointment.id, testResult.result),
         )
 
