e27dd966674ea81e0c07d2def545045bca610e82
Tsovak Harutyunyan <tsovak@stayopn.com>
2021-03-19 19:22:49 +0400

    fix: add new types to joe

--- packages/reservation/src/dbschemas/appointments.schema.ts
+++ packages/reservation/src/dbschemas/appointments.schema.ts
@@ -55,6 +55,16 @@ export default Joi.object({
   userId: Joi.string().allow(null),
   locationName: Joi.string().allow(''),
   locationAddress: Joi.string().allow(''),
-  testType: Joi.string().required().valid('PCR', 'RapidAntigen'),
+  testType: Joi.string()
+    .required()
+    .valid(
+      'PCR',
+      'RapidAntigen',
+      'Temperature',
+      'Attestation',
+      'EmergencyRapidAntigen',
+      'Antibody_All',
+      'Antibody_IgM',
+    ),
   labId: Joi.string().allow(null),
 })
