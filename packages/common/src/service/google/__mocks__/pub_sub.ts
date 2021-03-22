c26ce1630ce57271c28ea58816638f39c8fc579e
Harpreet Gill <harpreet@shopthing.com>
2021-03-22 18:43:06 -0400

    Make Passport Running

---
+++ packages/common/src/service/google/__mocks__/pub_sub.ts
@@ -0,0 +1,12 @@
+export class OPNPubSub {
+  constructor(topicName: string) {
+  }
+
+  async publish(data: unknown, attributes: Record<string, string> = {}): Promise<void> {
+    return
+  }
+
+  static async getPublishedData(data: string): Promise<unknown> {
+    return 
+  }
+}
