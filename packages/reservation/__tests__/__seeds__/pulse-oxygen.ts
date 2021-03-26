89127c8acba56a5eb01bb631110106b71cc2a631
Giorgi Gelashvili <g_gelashvili4@cu.edu.ge>
2021-03-26 18:15:22 +0400

    create api for pulse oxygen details

--- packages/reservation/__tests__/__seeds__/pulse-oxygen.ts
+++ packages/reservation/__tests__/__seeds__/pulse-oxygen.ts
@@ -3,6 +3,26 @@ import {firestore} from 'firebase-admin'
 const database = firestore()
 const collectionName = 'pulse-oxygen'
 
+export const createPulseOxygen = async (
+  id: string,
+  userId: string,
+  organizationId: string,
+): Promise<void> => {
+  const pulseOxygenCollection = database.collection(collectionName)
+  await pulseOxygenCollection.doc(id).set({
+    pulse: 90,
+    oxygen: 96,
+    organizationId,
+    locationId: 'TestLocation',
+    userId,
+    status: 'Passed',
+    timestamps: {
+      createdAt: firestore.Timestamp.fromDate(new Date()),
+      updatedAt: null,
+    },
+  })
+}
+
 export const deleteAllPulseOxygenByUserId = async (userId: string): Promise<void> => {
   const pulseOxygenCollection = database.collection(collectionName)
   const ref = await pulseOxygenCollection.where('userId', '==', userId).get()
