aa34fdf9f81059e1f9263c5193babce28a9d360d
Giorgi Gelashvili <g_gelashvili4@cu.edu.ge>
2021-03-29 16:28:50 +0400

    add enableTemperatureCheck scenarios

--- packages/reservation/__tests__/controllers/v1/admin/temperature.controller.ispec.ts
+++ packages/reservation/__tests__/controllers/v1/admin/temperature.controller.ispec.ts
@@ -4,10 +4,11 @@ import {app as server} from '../../../../src/app'
 
 jest.mock('../../../../../common/src/middlewares/authorization')
 jest.mock('../../../../../passport/src/services/attestation-service')
-jest.mock('../../../../../enterprise/src/services/organization-service')
 
 import {deleteAllPulseOxygenByUserId} from '../../../__seeds__/pulse-oxygen'
 import {deleteAllTemperatureByUserId} from '../../../__seeds__/temperature'
+import {createOrganization, deleteOrgById} from '../../../__seeds__/organization'
+import {createUser, deleteUserById} from '../../../__seeds__/user'
 
 const headers = {
   accept: 'application/json',
@@ -15,17 +16,42 @@ const headers = {
   Authorization: 'Bearer SuperAdmin',
 }
 
+const userId = 'USER1'
+const orgIdWithTempCheckEnabled = 'TestOrg1'
+const orgIdWithTempDisabled = 'TestOrg2'
+
 describe('Test temperature check request and update passport. Assume user has PROCEED as attestation status', () => {
+  beforeAll(async () => {
+    await createUser({
+      id: userId,
+      organizationIds: [orgIdWithTempCheckEnabled, orgIdWithTempDisabled],
+    })
+
+    await createOrganization({
+      id: orgIdWithTempCheckEnabled,
+      name: 'OrgWithTemperatureCheckEnabled',
+      enableTemperatureCheck: true,
+      userIdToAdd: userId,
+    })
+
+    await createOrganization({
+      id: 'TestOrg2',
+      name: 'OrgWithTemperatureCheckDisabled',
+      enableTemperatureCheck: false,
+      userIdToAdd: userId,
+    })
+  })
+
   test('should add new temperature & update passport status to PROCEED on normal oxygen & temperature', async (done) => {
     const url = '/reservation/admin/api/v1/temperature'
     const result = await request(server.app).post(url).set(headers).send({
-      organizationId: 'TEST1',
+      organizationId: orgIdWithTempCheckEnabled,
       temperature: 37,
       pulse: 90,
       oxygen: 97,
-      userId: 'TestUser',
+      userId,
     })
-
+    console.log({result})
     expect(result.body.data.status).toBe('proceed')
     done()
   })
@@ -33,11 +59,11 @@ describe('Test temperature check request and update passport. Assume user has PR
   test('should add new temperature & update passport status to STOP on LOW (92) oxygen', async (done) => {
     const url = '/reservation/admin/api/v1/temperature'
     const result = await request(server.app).post(url).set(headers).send({
-      organizationId: 'TEST1',
+      organizationId: orgIdWithTempCheckEnabled,
       temperature: 38,
       pulse: 90,
       oxygen: 92,
-      userId: 'TestUser',
+      userId,
     })
 
     expect(result.body.data.status).toBe('stop')
@@ -47,11 +73,11 @@ describe('Test temperature check request and update passport. Assume user has PR
   test('should add new temperature & update passport status to STOP on HIGH temperature', async (done) => {
     const url = '/reservation/admin/api/v1/temperature'
     const result = await request(server.app).post(url).set(headers).send({
-      organizationId: 'TEST1',
+      organizationId: orgIdWithTempCheckEnabled,
       temperature: 38,
       pulse: 90,
       oxygen: 96,
-      userId: 'TestUser',
+      userId,
     })
 
     expect(result.body.data.status).toBe('stop')
@@ -61,19 +87,38 @@ describe('Test temperature check request and update passport. Assume user has PR
   test('should add new temperature & update passport status to STOP LOW (92) oxygen & HIGH temperature', async (done) => {
     const url = '/reservation/admin/api/v1/temperature'
     const result = await request(server.app).post(url).set(headers).send({
-      organizationId: 'TEST1',
+      organizationId: orgIdWithTempCheckEnabled,
       temperature: 38,
       pulse: 90,
       oxygen: 96,
-      userId: 'TestUser',
+      userId,
     })
 
     expect(result.body.data.status).toBe('stop')
     done()
   })
 
-  afterAll(() => {
-    deleteAllPulseOxygenByUserId('TestUser')
-    deleteAllTemperatureByUserId('TestUser')
+  test('should return bad request status: TestOrg2 has disabled temperature check', async (done) => {
+    const url = '/reservation/admin/api/v1/temperature'
+    const result = await request(server.app).post(url).set(headers).send({
+      organizationId: orgIdWithTempDisabled,
+      temperature: 38,
+      pulse: 90,
+      oxygen: 96,
+      userId,
+    })
+
+    expect(result.status).toBe(400)
+    done()
+  })
+
+  afterAll(async () => {
+    await Promise.all([
+      deleteAllPulseOxygenByUserId(userId),
+      deleteAllTemperatureByUserId(userId),
+      deleteOrgById(orgIdWithTempCheckEnabled),
+      deleteOrgById(orgIdWithTempDisabled),
+      deleteUserById(userId),
+    ])
   })
 })
