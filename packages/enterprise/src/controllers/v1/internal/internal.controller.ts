ddc9922f3d719623b03281b0017d441bd441dc52
Maksym Perepelitsa <perepelitsa.maksym.external@kreios.lu>
2021-04-01 17:04:01 +0300

    Enhancement: 1836 fix lint issues

--- packages/enterprise/src/controllers/v1/internal/internal.controller.ts
+++ packages/enterprise/src/controllers/v1/internal/internal.controller.ts
@@ -2,27 +2,17 @@ import * as express from 'express'
 import {Handler, Router} from 'express'
 
 import IControllerBase from '../../../../../common/src/interfaces/IControllerBase.interface'
-import {OPNPubSub} from '../../../../../common/src/service/google/pub_sub'
 
-import {RecommendationService} from '../../../services/recommendation-service'
-import {StatusStatsService} from '../../../services/status-stats-service'
-import {OrganizationService} from '../../../services/organization-service'
-
-import {PassportStatus, PassportStatuses} from '../../../../../passport/src/models/passport'
+import {PassportStatus} from '../../../../../passport/src/models/passport'
 
 import {TemperatureStatuses} from '../../../../../reservation/src/models/temperature'
-import {
-  ResultTypes,
-  AppointmentStatus,
-  TestTypes,
-} from '../../../../../reservation/src/models/appointment'
+
+import {RecommendationService} from '../../../services/recommendation-service'
 import {AddPulse} from '../../../types/recommendations'
 
 class RecommendationController implements IControllerBase {
   public router = express.Router()
   private recService = new RecommendationService()
-  private statsService = new StatusStatsService()
-  private orgService = new OrganizationService()
   constructor() {
     this.initRoutes()
   }
