f07607fdb100a3351b1f35f2bfb947045abab97e
Artur Lymarenko <artur@stayopn.com>
2021-04-08 16:44:11 -0400

    fix: eslint config for services v2

---
+++ services-v2/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies ESLint parser
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2019, // Allows parsing of modern ECMA features
+    sourceType: 'module', // Allows for the use of imports
+    project: "./tsconfig.json"
+  },
+  rules: {
+    'max-params': [2,{ "max":3 }],
+    '@typescript-eslint/no-explicit-any': 2,
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {argsIgnorePattern: '^_', ignoreRestSiblings: true},
+    ],
+    '@typescript-eslint/ban-ts-comment': 'off',
+  },
+}
