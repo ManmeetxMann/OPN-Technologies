498fbcfa76caa7d5a2e74676be61cf5f52e0c775
Maksym Perepelitsa <perepelitsa.maksym.external@kreios.lu>
2021-04-08 15:47:50 +0300

    Feat: 1962 add content for result details

---
+++ migrations/add-result-details-content.ts
@@ -0,0 +1,33 @@
+/**
+ * This script add content
+ */
+import {initializeApp, credential, firestore} from 'firebase-admin'
+import {Config} from '../packages/common/src/utils/config'
+
+const serviceAccount = JSON.parse(Config.get('FIREBASE_ADMINSDK_SA'))
+initializeApp({
+  credential: credential.cert(serviceAccount),
+})
+
+console.log(serviceAccount.project_id)
+
+const database = firestore()
+
+async function main() {
+  database.collection('content').add({
+    contentType: 'result',
+    lang: 'en',
+    resultType: 'Negative',
+    details: `The result of your test was NEGATIVE. Your results do not detect SARS-CoV-2, the virus that causes coronavirus disease (also called COVID-19), a respiratory illness. A negative test means that the virus was not present in the sample we collected. Your results suggest you were negative at the time of testing.
+
+      Although the possibility is low, a false negative result should be considered if you have had recent exposure to the virus along with symptoms consistent with COVID-19.
+
+      If you are the patron receiving the test and require further information, please visit the City of Toronto Public Health: https://www.toronto.ca/home/covid-19
+
+      If you have further questions or concerns, you can contact FH Health at info@fhhealth.ca or (416) 484-0042.`,
+  })
+
+  return
+}
+
+main().then(() => console.log('Script Complete \n'))
