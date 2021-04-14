0805834b3d2ea264544f5a0ea727c50d02e5315b
Maksym Perepelitsa <perepelitsa.maksym.external@kreios.lu>
2021-04-14 19:03:34 +0300

    Bugfix: 1904 fixed lint issue

--- packages/passport/__tests__/controller/v1/passport.controller.is_pec.ts
+++ packages/passport/__tests__/controller/v1/passport.controller.is_pec.ts
@@ -2,9 +2,15 @@ import request from 'supertest'
 import {app as server} from '../../../src/server'
 
 import {createAttestation, deleteAttestationsById} from '../../__seeds__/attestation'
-import {createOrganization, deleteOrgById} from '../../../../reservation/__tests__/__seeds__/organization'
+import {
+  createOrganization,
+  deleteOrgById,
+} from '../../../../reservation/__tests__/__seeds__/organization'
 import {createQuestion, deleteQuestionById} from '../../__seeds__/questionnaires'
-import {createUser, deleteUserByIdTestDataCreator} from '../../../../reservation/__tests__/__seeds__/user'
+import {
+  createUser,
+  deleteUserByIdTestDataCreator,
+} from '../../../../reservation/__tests__/__seeds__/user'
 
 jest.spyOn(global.console, 'error').mockImplementation()
 jest.spyOn(global.console, 'info').mockImplementation()
@@ -14,48 +20,56 @@ jest.mock('../../../../common/src/utils/logging-setup')
 const testDataCreator = __filename.split('/packages/')[1]
 
 describe('Passport test suite', () => {
-    const dateForCreation = '2020-02-05'
-    const attestationId = 'IT_Attestation_1'
-    const questionnaireId = 'IT_Questionary_1'
-    const organizationId = 'IT_Organization_1'
-    const userId = 'IT_User_1'
+  const attestationId = 'IT_Attestation_1'
+  const questionnaireId = 'IT_Questionary_1'
+  const organizationId = 'IT_Organization_1'
+  const userId = 'IT_User_1'
 
-    beforeAll(async () => {
-        await Promise.all([
-            createUser({id: userId, organizationIds: [organizationId]}, testDataCreator),
-            createOrganization({
-                id: organizationId,
-                name: '401',
-                enableTemperatureCheck: true,
-                questionnaireId,
-                userIdToAdd: userId,
-            }, testDataCreator),
-            createQuestion({
-                id: questionnaireId
-            }, testDataCreator),
-            createAttestation({id: attestationId, userId, questionnaireId, organizationId}, testDataCreator)
-        ])
-    })
+  beforeAll(async () => {
+    await Promise.all([
+      createUser({id: userId, organizationIds: [organizationId]}, testDataCreator),
+      createOrganization(
+        {
+          id: organizationId,
+          name: '401',
+          enableTemperatureCheck: true,
+          questionnaireId,
+          userIdToAdd: userId,
+        },
+        testDataCreator,
+      ),
+      createQuestion(
+        {
+          id: questionnaireId,
+        },
+        testDataCreator,
+      ),
+      createAttestation(
+        {id: attestationId, userId, questionnaireId, organizationId},
+        testDataCreator,
+      ),
+    ])
+  })
 
-    test('Get attestation by ID 2', async (done) => {
-        const url = `passport/api/v1/attestation/${attestationId}`
-        const headers = {
-            authorization: `bearer bearer_token`,
-            organizationId,
-        }
+  test('Get attestation by ID 2', async (done) => {
+    const url = `passport/api/v1/attestation/${attestationId}`
+    const headers = {
+      authorization: `bearer bearer_token`,
+      organizationId,
+    }
 
-        const result = await request(server.app).get(url).set(headers)
-        expect(result.status).toBe(200)
+    const result = await request(server.app).get(url).set(headers)
+    expect(result.status).toBe(200)
 
-        done()
-    })
+    done()
+  })
 
-    afterAll(async () => {
-        await Promise.all([
-            deleteOrgById(organizationId, testDataCreator),
-            deleteAttestationsById(attestationId, testDataCreator),
-            deleteQuestionById(questionnaireId, testDataCreator),
-            deleteUserByIdTestDataCreator(userId, testDataCreator)
-        ]) 
-    })
+  afterAll(async () => {
+    await Promise.all([
+      deleteOrgById(organizationId, testDataCreator),
+      deleteAttestationsById(attestationId, testDataCreator),
+      deleteQuestionById(questionnaireId, testDataCreator),
+      deleteUserByIdTestDataCreator(userId, testDataCreator),
+    ])
+  })
 })
