b6c8f3b811505d90095a0a242e469ac59082451d
Giorgi Gelashvili <g_gelashvili4@cu.edu.ge>
2021-04-16 22:26:31 +0400

    add patient api controller & service, dtos

---
+++ services-v2/apps/user-service/src/controller/public/v1/patient.controller.ts
@@ -0,0 +1,82 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  NotFoundException,
+  Query,
+} from '@nestjs/common'
+import {ApiTags} from '@nestjs/swagger'
+import {ResponseWrapper} from '@opn/common/dto/response-wrapper'
+import {assignWithoutUndefined, ResponseStatusCodes} from '@opn/common/dto'
+import {Patient} from '../../../model/patient/patient.entity'
+import {
+  PatientCreateDto,
+  PatientFilter,
+  patientProfileDto,
+  PatientUpdateDto,
+} from '../../../dto/patient'
+import {PatientService} from '../../../service/patient/patient.service'
+import {FirebaseAuthService} from '@opn/common/services/auth/firebase-auth.service'
+
+@ApiTags('Patients')
+@Controller('/api/v1/patient')
+//TODO: auth & track updatedBy
+export class PatientController {
+  constructor(
+    private patientService: PatientService,
+    private firebaseAuthService: FirebaseAuthService,
+  ) {}
+
+  @Get()
+  async getAll(@Query() filter: PatientFilter): Promise<ResponseWrapper<Patient[]>> {
+    const {data, page, totalItems, totalPages} = await this.patientService.getAll(
+      assignWithoutUndefined(filter, new PatientFilter()),
+    )
+
+    return ResponseWrapper.of(data, ResponseStatusCodes.Succeed, null, page, totalPages, totalItems)
+  }
+
+  @Get('/:patientId')
+  async getById(@Param('patientId') id: string): Promise<ResponseWrapper<PatientUpdateDto>> {
+    const patient = await this.patientService.getProfilebyId(id)
+
+    if (!patient) {
+      throw new NotFoundException('User with given id not found')
+    }
+
+    return ResponseWrapper.actionSucceed(patientProfileDto(patient))
+  }
+
+  @Post()
+  async add(@Body() patientDto: PatientCreateDto): Promise<ResponseWrapper<Patient>> {
+    const patientExists = await this.firebaseAuthService.getUserByEmail(patientDto.email)
+
+    if (patientExists) {
+      throw new BadRequestException('User with given email already exists')
+    }
+
+    const patient = await this.patientService.createProfile(patientDto)
+
+    return ResponseWrapper.actionSucceed(patient)
+  }
+
+  @Put('/:patientId')
+  async update(
+    @Param('patientId') id: string,
+    @Body() patientUpdateDto: PatientUpdateDto,
+  ): Promise<ResponseWrapper> {
+    const patientExists = await this.patientService.getbyId(id)
+
+    if (!patientExists) {
+      throw new NotFoundException('User with given id not found')
+    }
+
+    await this.patientService.updateProfile(id, patientUpdateDto)
+
+    return ResponseWrapper.actionSucceed()
+  }
+}
--- services-v2/apps/user-service/src/main.ts
+++ services-v2/apps/user-service/src/main.ts
@@ -1,6 +1,6 @@
 // NestJs
 import {NestFactory} from '@nestjs/core'
-import {Module} from '@nestjs/common'
+import {Module, ValidationPipe} from '@nestjs/common'
 
 import {
   DatabaseConfiguration,
@@ -8,23 +8,31 @@ import {
 } from './configuration/database.configuration'
 
 import {AdminV1UserController} from './controller/admin/v1/user.controller'
+import {PatientController} from './controller/public/v1/patient.controller'
 
 import {UserService} from './service/user/user.service'
 import {OrganizationService} from './service/organization/organization.service'
 import {LocationService} from './service/organization/location.service'
 import {GroupService} from './service/organization/group.service'
+import {PatientService} from './service/patient/patient.service'
 
 import {CommonModule, createSwagger} from '@opn/common'
 
 @Module({
   imports: [CommonModule, DatabaseConfiguration, RepositoryConfiguration],
-  controllers: [AdminV1UserController],
-  providers: [OrganizationService, LocationService, GroupService, UserService],
+  controllers: [AdminV1UserController, PatientController],
+  providers: [OrganizationService, LocationService, GroupService, UserService, PatientService],
 })
 class App {}
 
 async function bootstrap() {
   const app = await NestFactory.create(App)
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidUnknownValues: true,
+    }),
+  )
 
   createSwagger(app)
 
