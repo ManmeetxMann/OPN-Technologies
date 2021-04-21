839f0a2132685a7a3dd270f2d5f7c09a2e085fc4
Artur Lymarenko <artur@stayopn.com>
2021-04-21 15:00:38 -0400

    fix: remove passport service

--- services-v2/apps/passport-service/src/model/access/access.ts
+++
@@ -1,9 +0,0 @@
-import {Auditable} from '@opn-services/common/model'
-
-export class Access extends Auditable {
-  token: string
-  locationId: string
-  userId: string
-  enteredAt: Date
-  exitedAt: Date
-}
