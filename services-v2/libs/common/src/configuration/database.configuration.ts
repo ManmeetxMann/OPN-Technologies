77fc3f65624832c565997439ec849e1215788d18
Artur Lymarenko <artur@stayopn.com>
2021-04-22 14:09:26 -0400

    fix: user cart connection, remove a logic for different db per service

--- services-v2/libs/common/src/configuration/database.configuration.ts
+++ services-v2/libs/common/src/configuration/database.configuration.ts
@@ -3,9 +3,7 @@ import {DynamicModule} from '@nestjs/common'
 import {ConfigModule, ConfigService} from '@nestjs/config'
 import {isRunningOnGCP} from '@opn-services/common/utils'
 
-export const DefaultDatabaseConfiguration = (service: string): DynamicModule => {
-  const env = (property: string) => [service, 'DB', property].join('_').toUpperCase()
-
+export const DefaultDatabaseConfiguration = (): DynamicModule => {
   return TypeOrmModule.forRootAsync({
     imports: [ConfigModule],
     inject: [ConfigService],
@@ -14,27 +12,27 @@ export const DefaultDatabaseConfiguration = (service: string): DynamicModule => 
       if (isRunningOnGCP()) {
         // Connect via socket when deployed to GCP
         connection = {
-          host: configService.get<string>(env('DB_SQL_HOST')),
+          host: configService.get<string>('DB_SQL_HOST'),
           extra: {
-            socketPath: configService.get<string>(env('DB_SQL_HOST')),
+            socketPath: configService.get<string>('DB_SQL_HOST'),
           },
         }
       } else {
         // Connect via TCP when on local with local DB or Cloud SQL with proxy
         connection = {
-          host: configService.get<string>(env('DB_SQL_LOCAL_HOST')),
-          port: configService.get<number>(env('DB_SQL_LOCAL_PORT')),
+          host: configService.get<string>('DB_SQL_LOCAL_HOST'),
+          port: configService.get<number>('DB_SQL_LOCAL_PORT'),
         }
       }
 
       return {
         ...connection,
         type: 'mysql',
-        database: configService.get<string>(env('DB_SQL_NAME')),
-        username: configService.get<string>(env('DB_SQL_USERNAME')),
-        password: configService.get<string>(env('DB_SQL_PASSWORD')),
-        synchronize: configService.get<boolean>(env('DB_SQL_AUTO_SYNC_SCHEMA')),
-        migrationsRun: configService.get<boolean>(env('DB_SQL_RUN_MIGRATION')),
+        database: configService.get<string>('DB_SQL_NAME'),
+        username: configService.get<string>('DB_SQL_USERNAME'),
+        password: configService.get<string>('DB_SQL_PASSWORD'),
+        synchronize: configService.get<boolean>('DB_SQL_AUTO_SYNC_SCHEMA'),
+        migrationsRun: configService.get<boolean>('DB_SQL_RUN_MIGRATION'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         logging: ['warn', 'error', 'query'],
         autoLoadEntities: true,
