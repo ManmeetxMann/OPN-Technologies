6b8b7b103b840d2f10bae9b1ccfd784f71d98ed9
Giorgi Gelashvili <g_gelashvili4@cu.edu.ge>
2021-04-23 18:11:59 +0400

    add bunyan logger from v1

--- services-v2/libs/common/src/utils/google-cloud.ts
+++ services-v2/libs/common/src/utils/google-cloud.ts
@@ -6,3 +6,11 @@ export const isRunningOnGCP = (): boolean => {
   const isRunningOnGCP = Boolean(configService.get('GOOGLE_CLOUD_PROJECT'))
   return isRunningOnGCP
 }
+
+export const NodeEnv = (): string => process.env.NODE_ENV
+
+//Only avaiable on Google App ENgine
+export const GAEService = (): string => process.env.GAE_SERVICE ?? 'local'
+
+//Only avaiable on Google App ENgine
+export const GAEProjectID = (): string => process.env.GOOGLE_CLOUD_PROJECT ?? 'local'
