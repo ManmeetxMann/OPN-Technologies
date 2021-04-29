a7026614703a3fbf9798ac8fef0241de127f00c7
Giorgi Gelashvili <g_gelashvili4@cu.edu.ge>
2021-04-29 15:47:36 +0400

    update script with transaction

--- tools/get-rapid-home-kit-code.ts
+++ tools/get-rapid-home-kit-code.ts
@@ -12,22 +12,48 @@ initializeApp({
   credential: credential.cert(serviceAccount),
 })
 
+const db = admin.firestore()
+const collection = db.collection('rapid-home-kit-codes')
+
+const countOfCodes = 2
+
 async function main() {
-  const db = admin.firestore()
-  const collection = db.collection('rapid-home-kit-codes')
-  const randomCode = await nanoid()
+  return db.runTransaction(async (transaction) => {
+    const codesToCreate = new Set<string>()
 
-  await collection.add({
-    code: randomCode,
-    printed: false,
+    /**
+     * Read operations
+     * Find collisions for every generated code
+     */
+    for (let i = 0; i < countOfCodes; i++) {
+      const randomCode = await nanoid()
+      const query = collection.where('code', '==', randomCode).limit(1)
+      const {docs} = await transaction.get(query)
+
+      if (docs.length) {
+        throw new Error(`Collision detected for code: ${randomCode}`)
+      }
+
+      codesToCreate.add(randomCode)
+    }
+
+    /**
+     * Write operations
+     * if collision doesn't exist create document
+     */
+    const results = Array.from(codesToCreate).map((code: string) => {
+      const codeDocument = collection.doc()
+      transaction.set(codeDocument, {code, printed: false})
+      return code
+    })
+
+    return results
   })
-
-  return randomCode
 }
 
 main()
-  .then(async (result) => {
-    const data = await result
-    console.log('Code Generated:', data)
+  .then((result) => {
+    console.log(`Created ${result.length} code with ${countOfCodes - result.length} collision`)
+    console.log('Codes Generated:', result)
   })
   .catch((e) => console.warn('Code Generation Failed: ', e))
