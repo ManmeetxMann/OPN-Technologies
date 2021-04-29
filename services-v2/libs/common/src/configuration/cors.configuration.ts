55cfacb40a3e860e9841b2da1570b2c56ff25a05
Giorgi Gelashvili <g_gelashvili4@cu.edu.ge>
2021-04-29 16:52:02 +0400

    update cors options with new config

--- services-v2/libs/common/src/configuration/cors.configuration.ts
+++ services-v2/libs/common/src/configuration/cors.configuration.ts
@@ -1,25 +1,10 @@
 import {CorsOptions} from '@nestjs/common/interfaces/external/cors-options.interface'
 import {ConfigService} from '@nestjs/config'
-import {GAEProjectID} from '../utils'
 
 const configService = new ConfigService()
 
-const getOriginsByEnv = () => {
-  const env = GAEProjectID()
-
-  switch (env) {
-    case 'ca-prod':
-      return configService.get('DASHBOARD_URL')
-    case 'preprod':
-      return configService.get('DASHBOARD_URL')
-    case 'local':
-      return '*'
-    default:
-      // default config for dev/infra envs
-      return '*'
-  }
-}
+const allowedOrigins = [configService.get('DASHBOARD_URL')]
 
 export const corsOptions: CorsOptions = {
-  origin: getOriginsByEnv(),
+  origin: allowedOrigins,
 }
