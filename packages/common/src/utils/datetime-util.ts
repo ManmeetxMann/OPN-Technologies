e7c0235feeb494224f443f70ac76cbefba21e2c9
Artur Lymarenko <artur@stayopn.com>
2021-04-29 13:03:27 -0400

    fix: unhandled v1 exception causing cart booking failure

--- packages/common/src/utils/datetime-util.ts
+++ packages/common/src/utils/datetime-util.ts
@@ -1,7 +1,9 @@
 import {now} from './times'
 
-import moment from 'moment-timezone'
 import {firestore} from 'firebase-admin'
+
+// Must be required, otherwise brake when imported to v2
+const moment = require('moment-timezone')
 
 // some timestamps are invalid and the "day" part is actually
 // the day of the year. This function accepts a (valid or invalid)
@@ -26,7 +28,7 @@ export type GenericTimestamp = string | moment.Moment | Date | firestore.FieldVa
 // handles strings with the "day of year" bug correctly
 export const safeTimestamp = (timestamp: GenericTimestamp): Date => {
   if (moment.isMoment(timestamp)) {
-    return timestamp.toDate()
+    return moment(timestamp).toDate()
   }
   if (typeof timestamp === 'number') {
     return new Date(timestamp)
