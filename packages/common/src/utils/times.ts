151024a3fa8f2460e25dd62112946d527cf6a253
Harpreet Gill <>
2021-05-13 00:11:21 -0400

    Remove SetTime Function

--- packages/common/src/utils/times.ts
+++ packages/common/src/utils/times.ts
@@ -19,18 +19,6 @@ export const serverTimestamp = (): firestore.FieldValue => {
   return firestore.FieldValue.serverTimestamp()
 }
 
-export const setTime: (req: Request, res: Response) => void = (
-  req: Request,
-  res: Response,
-): void => {
-  if (!manualModeEnabled) {
-    res.status(403).send('not in the correct mode for this')
-    return
-  }
-  currentTimeMillis = req.body.milliseconds
-  res.status(200).send('OK')
-}
-
 export const now = (): Date => {
   if (manualModeEnabled && currentTimeMillis) {
     return new Date(currentTimeMillis)
