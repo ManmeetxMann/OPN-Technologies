22a71e70e46b96188f374561601bb36026750a8f
Harpreet Gill <>
2021-05-17 17:28:52 -0400

    Added Entry Point Support

--- cloud-functions/src/hl7-egress/index.ts
+++ cloud-functions/src/hl7-egress/index.ts
@@ -1,9 +1,9 @@
 import type {EventFunctionWithCallback} from '@google-cloud/functions-framework/build/src/functions'
 
-const hl7Handler: EventFunctionWithCallback = async (pubSubMessage, context, callback) => {
+const requestHandler: EventFunctionWithCallback = async (pubSubMessage, context, callback) => {
   // tslint:disable: no-console
   console.log('Test')
   callback()
 }
 
-export {hl7Handler}
+export {requestHandler}
