e19ca1667c7e8533812ca81a8f6ff0fb19eb2881
Harpreet Gill <>
2021-05-21 13:55:11 -0400

    Update Lint

--- functional-testing/__tests__/service-v2/user-service/patients_post.js
+++ functional-testing/__tests__/service-v2/user-service/patients_post.js
@@ -19,21 +19,21 @@ const organizationId = testProfile.get().organizationId;
  */
 describe('post:patient', () => {
   test('create patient successfully', function() {
-    return helpersCommon.getAuthTokenByPhone(frisby,'+1 650-505-5050').then(function(token) {
+    return helpersCommon.getAuthTokenByPhone(frisby, '+1 650-505-5050').then(function(token) {
       const url = `${userServiceUrl}/api/v1/patients`;
       return frisby
           .setup({
             request: {
               headers: {
                 'Authorization': `Bearer ${token}`,
-                'organizationid':organizationId
+                'organizationid': organizationId,
               },
             },
           })
-          .post(url,{
-            'firstName':'HSG_FN1',
-            'lastName':'HSG_LN1',
-            'email':'harpreet+v2test1@stayopn.com',
+          .post(url, {
+            'firstName': 'HSG_FN1',
+            'lastName': 'HSG_LN1',
+            'email': 'harpreet+v2test1@stayopn.com',
           })
           .expect('status', 200)
           .inspectBody();
