0f22894570dc16d60080cf3021feb6645ddf1f50
Harpreet Gill <>
2021-05-28 14:03:04 -0400

    Fixed Integration test for new Framework

--- packages/reservation/__tests__/controllers/v1/admin/admin-scan-history.controller.bck.ts
+++ packages/reservation/__tests__/controllers/v1/admin/admin-scan-history.controller.bck.ts
@@ -50,7 +50,7 @@ describe('AdminScanHistoryController', () => {
   })
 
   describe('create scan history record', () => {
-    test('create new scan history record successfully', async (done) => {
+    test('create new scan history record successfully', async () => {
       const url = `/reservation/admin/api/v1/admin-scan-history`
       const result = await request(server.app)
         .post(url)
@@ -64,10 +64,9 @@ describe('AdminScanHistoryController', () => {
       expect(result.status).toBe(200)
       expect(result.body.data.id).toBe(aptID1)
       expect(result.body.data.status).toBe('InProgress')
-      done()
     })
 
-    test('create new scan history record fails for bad barcode', async (done) => {
+    test('create new scan history record fails for bad barcode', async () => {
       const url = `/reservation/admin/api/v1/admin-scan-history`
       const result = await request(server.app)
         .post(url)
@@ -79,10 +78,9 @@ describe('AdminScanHistoryController', () => {
         })
 
       expect(result.status).toBe(404)
-      done()
     })
 
-    test('create new scan history record fails for bad type', async (done) => {
+    test('create new scan history record fails for bad type', async () => {
       const url = `/reservation/admin/api/v1/admin-scan-history`
       const result = await request(server.app)
         .post(url)
@@ -94,12 +92,11 @@ describe('AdminScanHistoryController', () => {
         })
 
       expect(result.status).toBe(400)
-      done()
     })
   })
 
   describe('get scan history record', () => {
-    test('get new scan history record successfully', async (done) => {
+    test('get new scan history record successfully', async () => {
       const url = `/reservation/admin/api/v1/admin-scan-history?type=RapidAntigen`
       const result = await request(server.app)
         .get(url)
@@ -110,7 +107,6 @@ describe('AdminScanHistoryController', () => {
       expect(result.body.data).toEqual(
         expect.arrayContaining([expect.objectContaining({id: aptID1})]),
       )
-      done()
     })
   })
 
