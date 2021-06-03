0f6a43b4f68d3ae990170ac65328736003f79b1b
ArmondOPN <armond@stayopn.com>
2021-06-04 01:33:00 +0400

    FIX: update user when created coupon

--- services-v2/apps/user-service/src/service/patient/patient.service.ts
+++ services-v2/apps/user-service/src/service/patient/patient.service.ts
@@ -312,7 +312,7 @@ export class PatientService {
     const {travel, health, addresses, digitalConsent, auth} = patient
 
     if (auth && data.email && auth?.email !== data.email) {
-      this.firebaseAuthService.updateUser(auth.authUserId, data.email)
+      await this.firebaseAuthService.updateUser(auth.authUserId, {email: data.email})
       auth.email = data.email
       await this.patientAuthRepository.save(auth)
     }
