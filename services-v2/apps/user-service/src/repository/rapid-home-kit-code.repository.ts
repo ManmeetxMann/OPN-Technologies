3a542531bae208ccef0fd9ae1ccf50dbfbf87ec6
Artur Lymarenko <artur@stayopn.com>
2021-06-15 11:39:33 -0400

    fix: bugs around usedForUserId

--- services-v2/apps/user-service/src/repository/rapid-home-kit-code.repository.ts
+++ services-v2/apps/user-service/src/repository/rapid-home-kit-code.repository.ts
@@ -10,7 +10,7 @@ export type RapidHomeKitCode = Auditable & {
   printedOn: firestore.Timestamp
   userIds?: UserIds[]
   filterUserIds: string[]
-  userForUserId: string[]
+  usedForUserId : string[]
 }
 
 export type UserIds = {
