db575d5de422bf176f50b2f978c507ffb9f93548
Pavel Svintsitskiy <pavelsvintsitsky@gmail.com>
2021-06-19 01:59:02 +0300

    fix: well allow empty string

--- packages/reservation/src/dbschemas/test-runs-pool.schema.ts
+++ packages/reservation/src/dbschemas/test-runs-pool.schema.ts
@@ -4,6 +4,6 @@ export default Joi.object({
   poolBarCode: Joi.string().required(),
   testResultIds: Joi.array().required(),
   testRunId: Joi.string().required(),
-  well: Joi.string().required(),
+  well: Joi.string().allow(''),
   numberOfSamples: Joi.number().required(),
 })
